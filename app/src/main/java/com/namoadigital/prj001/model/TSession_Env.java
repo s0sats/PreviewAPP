ae41cdaa572008293307e614df78eab818341951
DanielLuche <d.luche@namoadigital.com>
2017-01-20 11:45:22 -0200

    Modelo de Envio do ws get_session TSession_Env

---
+++ app/src/main/java/com/namoadigital/prj001/model/TSession_Env.java
@@ -0,0 +1,127 @@
+package com.namoadigital.prj001.model;
+
+/**
+ * Created by DANIEL.LUCHE on 20/01/2017.
+ * Classe com propriedade que serão enviadas para o
+ * servidor na chamada do WS server_get_session.
+ */
+public class TSession_Env {
+
+    private String app_code;
+    private String app_version;
+    private String email_p;
+    private String password;
+    private String nfc_code;
+    private String device_code;
+    private String manufacturer;
+    private String model;
+    private String version_os;
+    private String os;
+    private String force_login;
+    private String customer_code;
+    private int translate_code;
+
+    public String getApp_code() {
+        return app_code;
+    }
+
+    public void setApp_code(String app_code) {
+        this.app_code = app_code;
+    }
+
+    public String getApp_version() {
+        return app_version;
+    }
+
+    public void setApp_version(String app_version) {
+        this.app_version = app_version;
+    }
+
+    public String getEmail_p() {
+        return email_p;
+    }
+
+    public void setEmail_p(String email_p) {
+        this.email_p = email_p;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNfc_code() {
+        return nfc_code;
+    }
+
+    public void setNfc_code(String nfc_code) {
+        this.nfc_code = nfc_code;
+    }
+
+    public String getDevice_code() {
+        return device_code;
+    }
+
+    public void setDevice_code(String device_code) {
+        this.device_code = device_code;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getVersion_os() {
+        return version_os;
+    }
+
+    public void setVersion_os(String version_os) {
+        this.version_os = version_os;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public String getForce_login() {
+        return force_login;
+    }
+
+    public void setForce_login(String force_login) {
+        this.force_login = force_login;
+    }
+
+    public String getCustomer_code() {
+        return customer_code;
+    }
+
+    public void setCustomer_code(String customer_code) {
+        this.customer_code = customer_code;
+    }
+
+    public int getTranslate_code() {
+        return translate_code;
+    }
+
+    public void setTranslate_code(int translate_code) {
+        this.translate_code = translate_code;
+    }
+}
