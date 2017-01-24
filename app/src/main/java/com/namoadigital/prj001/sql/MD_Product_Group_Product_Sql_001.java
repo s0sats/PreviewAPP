1509aeed497b5be42c4d2e6e91727d9aa729e6e2
DanielLuche <d.luche@namoadigital.com>
2017-01-24 10:04:50 -0200

    Criado Sql_001 para as tabelas  md_product_groups e md_product_group_products

---
+++ app/src/main/java/com/namoadigital/prj001/sql/MD_Product_Group_Product_Sql_001.java
@@ -0,0 +1,45 @@
+package com.namoadigital.prj001.sql;
+
+import com.namoadigital.prj001.dao.MD_Product_Group_ProductDao;
+import com.namoadigital.prj001.database.Specification;
+
+/**
+ * Created by DANIEL.LUCHE on 24/01/2017.
+ */
+
+public class MD_Product_Group_Product_Sql_001 implements Specification {
+
+    private long s_customer_code;
+    private long s_group_code;
+    private long s_product_code;
+
+    public MD_Product_Group_Product_Sql_001(long s_customer_code, long s_group_code, long s_product_code) {
+        this.s_customer_code = s_customer_code;
+        this.s_group_code = s_group_code;
+        this.s_product_code = s_product_code;
+    }
+
+
+    @Override
+    public String toSqlQuery() {
+        StringBuilder sb = new StringBuilder();
+
+        return sb.append(
+                "SELECT " +
+                "   * " +
+                "FROM ")
+                .append(MD_Product_Group_ProductDao.TABLE)
+                .append(" WHERE " +
+                        MD_Product_Group_ProductDao.CUSTOMER_CODE +" = '"+s_customer_code+"'"+
+                        " AND " +
+                        MD_Product_Group_ProductDao.GROUP_CODE +" = '"+s_group_code+"'"+
+                        " AND " +
+                        MD_Product_Group_ProductDao.PRODUCT_CODE +" = '"+s_product_code+"'"+
+                        "ORDER BY " +
+                        MD_Product_Group_ProductDao.GROUP_CODE + " , " +
+                        MD_Product_Group_ProductDao.PRODUCT_CODE
+                )
+                .append(";customer_code#group_code#product_code")
+                .toString();
+    }
+}
