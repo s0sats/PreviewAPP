e5e52f50aec7a7a90270ec36ac1c310e5e20e55f
DanielLuche <d.luche@namoadigital.com>
2017-01-31 17:32:23 -0200

    Tradução dos itens de menu Modificado Act003 e 004  - Removido Footer  - Criado metodos para tradução
    Toolbox_Con
    Add metodos preferenciade Sesssion App
    
    Act005
      - Criadas constantes das Tag do HMAux
      - Metodo que analisa o click e define qual tela chamar
      - Programado UpdateRequired
       - Programado ProcessGo
       - Metodo que chama Act006
    Add Interface e layout do Fragment

--- app/src/main/java/com/namoadigital/prj001/ui/act005/Act005_Opc.java
+++ app/src/main/java/com/namoadigital/prj001/ui/act005/Act005_Opc.java
@@ -6,8 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
 
+import com.namoa_digital.namoa_library.util.HMAux;
 import com.namoadigital.prj001.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by DANIEL.LUCHE on 31/01/2017.
@@ -15,15 +22,65 @@ import com.namoadigital.prj001.R;
 
 public class Act005_Opc extends Fragment {
 
+    private ImageView iv_logo;
+    private ListView lv_opc;
+    private IAct005_Opc delegate;
+
+    public interface IAct005_Opc{
+        void itemClicked(int index);
+    }
+
+    public void setOnOpcItemClicked(IAct005_Opc delegate) {
+        this.delegate = delegate;
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.act005_opc_content,container,false);
         //
-        //view.findViewById()
+        iniVar(view);
+        iniAction(view);
         //
         return view;
+
+    }
+
+    private void iniAction(View view) {
+
+        lv_opc.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(delegate != null){
+                    HMAux item = (HMAux) parent.getItemAtPosition(position);
+                    delegate.itemClicked(Integer.parseInt(item.get("id")));
+                }
+            }
+        });
+    }
+
+    private void iniVar(View view) {
+        iv_logo = (ImageView) view.findViewById(R.id.act005_opc_iv_logo);
+        //
+        lv_opc = (ListView) view.findViewById(R.id.act005_opc_lv_opt);
+
+        loadOptions();
+    }
+
+    private void loadOptions() {
+        String[] id = {"1"};
+        int[] icon = {R.drawable.cloud_upload};
+        String[] desc = {"Site"};
+        List<HMAux> drawerItemList =  new ArrayList<>();
+
+        for (int i = 0; i < id.length;i++){
+            HMAux hmAux =  new HMAux();
+            hmAux.put("id",id[i]);
+            hmAux.put("icon", String.valueOf(icon[i]));
+            hmAux.put("desc",desc[i]);
+            drawerItemList.add(hmAux);
+        }
 
     }
 }
