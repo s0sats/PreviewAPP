63a0e50862d79904a354f526546539d0635495c8
DanielLuche <d.luche@namoadigital.com>
2017-03-14 17:03:23 -0300

    Ataulização layout 2  - Local_Data_List_Adapter   - Modificado layout de fundo e feito FindViewById do itens da tela.   Continuar

--- app/src/main/java/com/namoadigital/prj001/adapter/Local_Data_List_Adapter.java
+++ app/src/main/java/com/namoadigital/prj001/adapter/Local_Data_List_Adapter.java
@@ -14,6 +14,7 @@ import com.namoadigital.prj001.R;
 import com.namoadigital.prj001.dao.GE_Custom_Form_LocalDao;
 import com.namoadigital.prj001.util.Constant;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -56,68 +57,103 @@ public class Local_Data_List_Adapter extends BaseAdapter {
             convertView = mInflater.inflate(resource,parent,false);
 
         }
-        //Inicializa variaveis do layout da celula
-        LinearLayout llBackground = (LinearLayout) convertView.findViewById(R.id.local_data_list_cell_01_ll_bg);
-        //
-        TextView tvItem = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_01);
-        //
-        TextView tvItem2 = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_02);
-        //
-        TextView tvItem3 = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_03);
+
         //Resgata HmAux com as informações
         HMAux item = source.get(position);
 
+        //Inicializa variaveis do layout da celula
+        LinearLayout llBackground = (LinearLayout) convertView.findViewById(R.id.local_data_list_cell_01_ll_bg);
+        //
+        ArrayList<TextView> tv_list = new ArrayList<>();
+        //
+        TextView tv_date_lvl = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_date_label);
+        TextView tv_date_val = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_date);
+        tv_list.add(tv_date_lvl);
+        tv_list.add(tv_date_val);
+        //
+        TextView tv_hour_lvl = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_hour_label);
+        TextView tv_hour_val = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_hour);
+        tv_list.add(tv_hour_lvl);
+        tv_list.add(tv_hour_val);
+        //
+        TextView tv_product_ttl = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_prod_ttl);
+        tv_list.add(tv_product_ttl);
+        //
+        TextView tv_code_lvl = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_code_label);
+        TextView tv_code_val = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_code_val);
+        tv_list.add(tv_code_lvl);
+        tv_list.add(tv_code_val);
+        //
+        TextView tv_id_lvl = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_id_label);
+        TextView tv_id_val = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_id_val);
+        tv_list.add(tv_id_lvl);
+        tv_list.add(tv_id_val);
+        //
+        TextView tv_product_desc_lbl = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_prod_desc_lbl);
+        TextView tv_product_desc_val = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_prod_desc_val);
+        tv_list.add(tv_product_desc_lbl);
+        tv_list.add(tv_product_desc_val);
+        //
+        TextView tv_form_ttl = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_form_ttl);
+        tv_list.add(tv_form_ttl);
+        //
+        TextView tv_type_lbl = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_type_label);
+        TextView tv_type_val = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_type_val);
+        tv_list.add(tv_type_lbl);
+        tv_list.add(tv_type_val);
+        //
+        TextView tv_form_lbl = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_form_label);
+        TextView tv_form_val = (TextView) convertView.findViewById(R.id.local_data_list_cell_01_tv_form_val);
+        tv_list.add(tv_form_lbl);
+        tv_list.add(tv_form_val);
+        //
         Drawable llDrawable = null;
 
-        tvItem.setText(
-                item.get(GE_Custom_Form_LocalDao.CUSTOM_PRODUCT_CODE) +
-                " - " + item.get(GE_Custom_Form_LocalDao.CUSTOM_PRODUCT_DESC));
-        tvItem2.setText(
-                item.get(
-                        GE_Custom_Form_LocalDao.CUSTOM_FORM_TYPE) +
-                        " - " + item.get(GE_Custom_Form_LocalDao.CUSTOM_FORM_TYPE_DESC) +
-                        " - " + item.get(GE_Custom_Form_LocalDao.CUSTOM_FORM_CODE) +
-                        " - " + item.get(GE_Custom_Form_LocalDao.CUSTOM_FORM_VERSION) +
-                        " - " + item.get(GE_Custom_Form_LocalDao.CUSTOM_FORM_DESC)
-        );
-        tvItem3.setText("# " +
-                item.get(GE_Custom_Form_LocalDao.CUSTOM_FORM_DATA)
-        );
+//        tvItem.setText(
+//                item.get(GE_Custom_Form_LocalDao.CUSTOM_PRODUCT_CODE) +
+//                " - " + item.get(GE_Custom_Form_LocalDao.CUSTOM_PRODUCT_DESC));
+//        tvItem2.setText(
+//                item.get(
+//                        GE_Custom_Form_LocalDao.CUSTOM_FORM_TYPE) +
+//                        " - " + item.get(GE_Custom_Form_LocalDao.CUSTOM_FORM_TYPE_DESC) +
+//                        " - " + item.get(GE_Custom_Form_LocalDao.CUSTOM_FORM_CODE) +
+//                        " - " + item.get(GE_Custom_Form_LocalDao.CUSTOM_FORM_VERSION) +
+//                        " - " + item.get(GE_Custom_Form_LocalDao.CUSTOM_FORM_DESC)
+//        );
+//        tvItem3.setText("# " +
+//                item.get(GE_Custom_Form_LocalDao.CUSTOM_FORM_DATA)
+//        );
 
         switch (item.get(GE_Custom_Form_LocalDao.CUSTOM_FORM_STATUS)){
 
             case Constant.CUSTOM_FORM_STATUS_IN_PROCESSING:
                 llDrawable = context.getResources().getDrawable(R.drawable.act013_cell_in_processing_states);
                 llBackground.setBackground(llDrawable);
-                tvItem.setTextColor(context.getResources().getColorStateList(R.color.custom_yellow_dark));
-                tvItem2.setTextColor(context.getResources().getColorStateList(R.color.custom_yellow_dark));
-                tvItem3.setTextColor(context.getResources().getColorStateList(R.color.custom_yellow_dark));
-
                 break;
             //
             case Constant.CUSTOM_FORM_STATUS_FINALIZED:
-                llDrawable = context.getResources().getDrawable(R.drawable.namoa_cell_states);
+                llDrawable = context.getResources().getDrawable(R.drawable.namoa_cell_4_states);
                 llBackground.setBackground(llDrawable);
-                tvItem.setTextColor(context.getResources().getColorStateList(R.color.namoa_cell_font_color));
-                tvItem2.setTextColor(context.getResources().getColorStateList(R.color.namoa_cell_font_color));
-                tvItem3.setTextColor(context.getResources().getColorStateList(R.color.namoa_cell_font_color));
+//                tvItem.setTextColor(context.getResources().getColorStateList(R.color.namoa_color_dark_blue));
+//                tvItem2.setTextColor(context.getResources().getColorStateList(R.color.namoa_color_dark_blue));
+//                tvItem3.setTextColor(context.getResources().getColorStateList(R.color.namoa_color_dark_blue));
 
                 break;
             case Constant.CUSTOM_FORM_STATUS_SENT:
                 llDrawable = context.getResources().getDrawable(R.drawable.act013_cell_sent_states);
                 llBackground.setBackground(llDrawable);
-                tvItem.setTextColor(context.getResources().getColorStateList(R.color.namoa_color_dark_blue));
-                tvItem2.setTextColor(context.getResources().getColorStateList(R.color.namoa_color_dark_blue));
-                tvItem3.setTextColor(context.getResources().getColorStateList(R.color.namoa_color_dark_blue));
+//                tvItem.setTextColor(context.getResources().getColorStateList(namoa_color_dark_blue));
+//                tvItem2.setTextColor(context.getResources().getColorStateList(namoa_color_dark_blue));
+//                tvItem3.setTextColor(context.getResources().getColorStateList(namoa_color_dark_blue));
 
                 break;
 
             default:
                 llDrawable = context.getResources().getDrawable(R.drawable.lib_custom_cell_bg_base);
                 llBackground.setBackground(llDrawable);
-                tvItem.setTextColor(context.getResources().getColorStateList(R.color.lib_custom_cell_font_color));
-                tvItem2.setTextColor(context.getResources().getColorStateList(R.color.lib_custom_cell_font_color));
-                tvItem3.setTextColor(context.getResources().getColorStateList(R.color.lib_custom_cell_font_color));
+ //               tvItem.setTextColor(context.getResources().getColorStateList(R.color.lib_custom_cell_font_color));
+ //               tvItem2.setTextColor(context.getResources().getColorStateList(R.color.lib_custom_cell_font_color));
+ //               tvItem3.setTextColor(context.getResources().getColorStateList(R.color.lib_custom_cell_font_color));
 
                 break;
         }
