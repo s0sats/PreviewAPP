98d857f03e29abbe18e2a25c35921c1c77df0cb8
DanielLuche <d.luche@namoadigital.com>
2017-03-24 20:42:44 -0300

    Ataualizado versão do ap

--- app/src/main/java/com/namoadigital/prj001/util/Constant.java
+++ app/src/main/java/com/namoadigital/prj001/util/Constant.java
@@ -12,7 +12,7 @@ public class Constant extends ConstantBase {
      * PROJECT ID - PRJ001 - SMS
      */
     public static final String PRJ001_CODE = "PRJ001";
-    public static final String PRJ001_VERSION = "0.8";
+    public static final String PRJ001_VERSION = "0.9";
 
     /**
      * UI Module
