3e3c8bc3603837dfc54e24d5f6c98e1b189dabe5
DanielLuche <d.luche@namoadigital.com>
2017-03-30 12:00:53 -0300

    Criando Ev_User_Customer_Parameter Setup   - Modelo   - Dao   - DatabaseHelper   - Sql

---
+++ app/src/main/java/com/namoadigital/prj001/dao/Ev_User_Customer_ParameterDao.java
@@ -0,0 +1,254 @@
+package com.namoadigital.prj001.dao;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+
+import com.namoa_digital.namoa_library.util.HMAux;
+import com.namoadigital.prj001.database.CursorToHMAuxMapper;
+import com.namoadigital.prj001.database.Mapper;
+import com.namoadigital.prj001.model.Ev_User_Customer_Parameter;
+import com.namoadigital.prj001.util.Constant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by DANIEL.LUCHE on 30/03/2017.
+ */
+
+public class Ev_User_Customer_ParameterDao extends BaseDao implements Dao<Ev_User_Customer_Parameter> {
+
+    private final Mapper<Ev_User_Customer_Parameter, ContentValues> toContentValuesMapper;
+    private final Mapper<Cursor, Ev_User_Customer_Parameter> toEv_User_Customer_ParameterMapper;
+
+    public static final String TABLE = "ev_user_customer_parameters";
+    public static final String CUSTOMER_CODE = "customer_code";
+    public static final String PARAMETER_CODE = "parameter_code";
+
+    public Ev_User_Customer_ParameterDao(Context context,String DB_NAME, int DB_VERSION) {
+        super(context, DB_NAME, DB_VERSION, Constant.DB_MODE_SINGLE );
+
+        this.toContentValuesMapper = new Ev_User_Customer_ParameterToContentValuesMapper();
+        this.toEv_User_Customer_ParameterMapper = new CursorToEv_User_Customer_ParameterMapper();
+
+    }
+
+
+    @Override
+    public void addUpdate(Ev_User_Customer_Parameter customer_parameter) {
+        openDB();
+
+        try {
+
+            if (db.insert(TABLE, null, toContentValuesMapper.map(customer_parameter)) == -1) {
+                StringBuilder sbWhere = new StringBuilder();
+                sbWhere.append(CUSTOMER_CODE).append(" = '").append(String.valueOf(customer_parameter.getCustomer_code())).append("'");
+
+                db.update(TABLE, toContentValuesMapper.map(customer_parameter), sbWhere.toString(), null);
+            }
+
+        } catch (Exception e) {
+        } finally {
+        }
+        closeDB();
+    }
+
+    @Override
+    public void addUpdate(Iterable<Ev_User_Customer_Parameter> customer_parameters, boolean status) {
+
+        openDB();
+
+        try {
+
+            db.beginTransaction();
+
+            if (status) {
+                db.delete(TABLE, null, null);
+            }
+
+            for (Ev_User_Customer_Parameter customer_parameter : customer_parameters) {
+                if (db.insert(TABLE, null, toContentValuesMapper.map(customer_parameter)) == -1) {
+                    StringBuilder sbWhere = new StringBuilder();
+                    sbWhere.append(CUSTOMER_CODE).append(" = '").append(String.valueOf(customer_parameter.getCustomer_code())).append("'");
+
+                    db.update(TABLE, toContentValuesMapper.map(customer_parameter), sbWhere.toString(), null);
+                }
+            }
+
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+        } finally {
+            db.endTransaction();
+        }
+        closeDB();
+
+    }
+
+    @Override
+    public void addUpdate(String sQuery) {
+        openDB();
+
+        try {
+
+            db.execSQL(sQuery);
+
+        } catch (Exception e) {
+        } finally {
+        }
+
+        closeDB();
+    }
+
+    @Override
+    public void remove(String sQuery) {
+        openDB();
+
+        try {
+
+            db.execSQL(sQuery);
+
+        } catch (Exception e) {
+        } finally {
+        }
+
+        closeDB();
+    }
+
+    @Override
+    public Ev_User_Customer_Parameter getByString(String sQuery) {
+        Ev_User_Customer_Parameter customer_parameter = null;
+
+        openDB();
+
+        try {
+
+            Cursor cursor = db.rawQuery(sQuery, null);
+
+            while (cursor.moveToNext()) {
+                customer_parameter = toEv_User_Customer_ParameterMapper.map(cursor);
+            }
+
+            cursor.close();
+        } catch (Exception e) {
+
+        } finally {
+        }
+
+        closeDB();
+
+        return customer_parameter;
+    }
+
+    @Override
+    public HMAux getByStringHM(String sQuery) {
+        HMAux hmAux = null;
+        openDB();
+
+        String s_query_div[] = sQuery.split(";");
+
+        Mapper<Cursor, HMAux> toHMAuxMapper = new CursorToHMAuxMapper(s_query_div[1]);
+
+        try {
+
+            Cursor cursor = db.rawQuery(s_query_div[0], null);
+
+            while (cursor.moveToNext()) {
+                hmAux = toHMAuxMapper.map(cursor);
+            }
+
+            cursor.close();
+        } catch (Exception e) {
+
+        } finally {
+        }
+
+        closeDB();
+
+        return hmAux;
+    }
+
+    @Override
+    public List<Ev_User_Customer_Parameter> query(String sQuery) {
+        List<Ev_User_Customer_Parameter> customer_parameters =  new ArrayList<>();
+        openDB();
+
+        try {
+
+            Cursor cursor = db.rawQuery(sQuery, null);
+
+            while (cursor.moveToNext()) {
+                Ev_User_Customer_Parameter uAux = toEv_User_Customer_ParameterMapper.map(cursor);
+                customer_parameters.add(uAux);
+            }
+
+            cursor.close();
+        } catch (Exception e) {
+
+        } finally {
+        }
+
+        closeDB();
+
+        return customer_parameters;
+    }
+
+    @Override
+    public List<HMAux> query_HM(String sQuery) {
+
+        List<HMAux> customer_parameters = new ArrayList<>();
+
+        openDB();
+
+        String s_query_div[] = sQuery.split(";");
+
+        Mapper<Cursor, HMAux> toHMAuxMapper = new CursorToHMAuxMapper(s_query_div[1]);
+
+        try {
+
+            Cursor cursor = db.rawQuery(s_query_div[0], null);
+
+            while (cursor.moveToNext()) {
+                customer_parameters.add(toHMAuxMapper.map(cursor));
+            }
+
+            cursor.close();
+        } catch (Exception e) {
+            String st = e.toString();
+        } finally {
+        }
+
+        closeDB();
+
+
+
+        return customer_parameters;
+    }
+
+    private class CursorToEv_User_Customer_ParameterMapper implements Mapper<Cursor,Ev_User_Customer_Parameter> {
+        @Override
+        public Ev_User_Customer_Parameter map(Cursor cursor) {
+            Ev_User_Customer_Parameter customerParameter =  new Ev_User_Customer_Parameter();
+            customerParameter.setCustomer_code(cursor.getLong(cursor.getColumnIndex(CUSTOMER_CODE)));
+            customerParameter.setParameter_code(cursor.getString(cursor.getColumnIndex(PARAMETER_CODE)));
+            return customerParameter;
+        }
+    }
+
+    private class Ev_User_Customer_ParameterToContentValuesMapper implements Mapper<Ev_User_Customer_Parameter,ContentValues> {
+        @Override
+        public ContentValues map(Ev_User_Customer_Parameter ev_user_customer_parameter) {
+            ContentValues contentValues = new ContentValues();
+
+            if(ev_user_customer_parameter.getCustomer_code() > -1){
+                contentValues.put(CUSTOMER_CODE,ev_user_customer_parameter.getCustomer_code());
+            }
+            if(ev_user_customer_parameter.getParameter_code() != null){
+                contentValues.put(PARAMETER_CODE,ev_user_customer_parameter.getParameter_code());
+            }
+
+            return contentValues;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/namoadigital/prj001/model/Ev_User_Customer_Parameter.java
@@ -0,0 +1,28 @@
+package com.namoadigital.prj001.model;
+
+/**
+ * Created by DANIEL.LUCHE on 30/03/2017.
+ */
+
+public class Ev_User_Customer_Parameter {
+
+    private long customer_code;
+    private String parameter_code;
+
+
+    public long getCustomer_code() {
+        return customer_code;
+    }
+
+    public void setCustomer_code(long customer_code) {
+        this.customer_code = customer_code;
+    }
+
+    public String getParameter_code() {
+        return parameter_code;
+    }
+
+    public void setParameter_code(String parameter_code) {
+        this.parameter_code = parameter_code;
+    }
+}
