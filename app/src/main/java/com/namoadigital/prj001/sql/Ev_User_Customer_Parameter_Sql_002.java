efd4b3440d23a974f2e4984347a3595c800c1ecf
DanielLuche <d.luche@namoadigital.com>
2017-03-30 13:52:37 -0300

    Finalzado  Ev_User_Customer_Parameter Setup   - Modelo   - Dao   - DatabaseHelper   - Sql FALTA TESTAR

--- app/src/main/java/com/namoadigital/prj001/sql/Ev_User_Customer_Parameter_Sql_002.java
+++ app/src/main/java/com/namoadigital/prj001/sql/Ev_User_Customer_Parameter_Sql_002.java
@@ -7,11 +7,16 @@ import com.namoadigital.prj001.database.Specification;
  * Created by DANIEL.LUCHE on 30/03/2017.
  */
 
-public class Ev_User_Customer_Parameter_Sql_001 implements Specification {
+/**
+ * Busca parametro no tabela
+ * Se retorno != null , tem permissão.
+ */
+
+public class Ev_User_Customer_Parameter_Sql_002 implements Specification {
 
     private String customer_code;
 
-    public Ev_User_Customer_Parameter_Sql_001(String customer_code) {
+    public Ev_User_Customer_Parameter_Sql_002(String customer_code) {
         this.customer_code = customer_code;
     }
 
@@ -22,11 +27,10 @@ public class Ev_User_Customer_Parameter_Sql_001 implements Specification {
                 .append(" SELECT\n " +
                         "   cp.* \n" +
                         " FROM \n " +
-                                Ev_User_Customer_ParameterDao.TABLE +" cp \n" +
+                        Ev_User_Customer_ParameterDao.TABLE +" cp \n " +
                         " WHERE \n" +
                         "  cp.customer_code = '"+customer_code+"' \n " +
-                        " ORDER BY \n" +
-                        "  cp.parameter_code")
+                        "  AND cp.parameter_code = '"+customer_code+"'")
                 .toString();
     }
 }
