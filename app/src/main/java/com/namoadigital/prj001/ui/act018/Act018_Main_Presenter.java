dd83d63773b382adda29892a92440b1f84f1b1e9
N.Hugo <neonhugo@gmail.com>
2017-04-17 16:40:42 -0300

    ACT018 ( Lista de Mensagens )

---
+++ app/src/main/java/com/namoadigital/prj001/ui/act018/Act018_Main_Presenter.java
@@ -0,0 +1,16 @@
+package com.namoadigital.prj001.ui.act018;
+
+import com.namoa_digital.namoa_library.util.HMAux;
+
+/**
+ * Created by DANIEL.LUCHE on 24/02/2017.
+ */
+
+public interface Act018_Main_Presenter {
+
+    void getSentData();
+
+    void addFormInfoToBundle(HMAux item);
+
+    void onBackPressedClicked();
+}
---
+++ app/src/main/java/com/namoadigital/prj001/ui/act018/Act018_Main_Presenter_Impl.java
@@ -0,0 +1,25 @@
+package com.namoadigital.prj001.ui.act018;
+
+import com.namoa_digital.namoa_library.util.HMAux;
+
+/**
+ * Created by neomatrix on 17/04/17.
+ */
+
+public class Act018_Main_Presenter_Impl implements Act018_Main_Presenter {
+
+    @Override
+    public void getSentData() {
+
+    }
+
+    @Override
+    public void addFormInfoToBundle(HMAux item) {
+
+    }
+
+    @Override
+    public void onBackPressedClicked() {
+
+    }
+}
