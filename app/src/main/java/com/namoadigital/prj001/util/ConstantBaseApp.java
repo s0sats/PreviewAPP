706d22067a1d9a96b7ce65c7026f33482596a208
DanielLuche <d.luche@namoadigital.com>
2017-05-17 16:44:13 -0300

    Toolbox_Inf  -add tratativa do retorno de login NFC_BLOCKED

--- app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
+++ app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
@@ -13,7 +13,7 @@ public class ConstantBaseApp  extends ConstantBase {
      * PROJECT ID - PRJ001 - SMS
      */
     public static final String PRJ001_CODE = "PRJ001";
-    public static final String PRJ001_VERSION = "1.06";
+    public static final String PRJ001_VERSION = "1.07";
 
     /**
      * UI Module
