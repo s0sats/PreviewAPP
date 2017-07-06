32623d0d7261f391940f5752940ffa1df37c7621
N.Hugo <neonhugo@gmail.com>
2017-07-06 16:14:01 -0300

    SqlSpecification for SO Files

--- app/src/main/java/com/namoadigital/prj001/sql/SM_SO_File_Sql_001.java
+++ app/src/main/java/com/namoadigital/prj001/sql/SM_SO_File_Sql_001.java
@@ -1,22 +1,24 @@
 package com.namoadigital.prj001.sql;
 
-import com.namoadigital.prj001.dao.SM_SODao;
+import com.namoadigital.prj001.dao.SM_SO_FileDao;
 import com.namoadigital.prj001.database.Specification;
 
 /**
- * Created by d.luche on 21/06/2017.
+ * Created by neomatrix on 06/07/17.
  */
 
-public class SM_SO_Sql_001 implements Specification {
+public class SM_SO_File_Sql_001 implements Specification {
 
     private long customer_code;
     private int so_prefix;
     private int so_code;
+    private int file_code;
 
-    public SM_SO_Sql_001(long customer_code, int so_prefix, int so_code) {
+    public SM_SO_File_Sql_001(long customer_code, int so_prefix, int so_code, int file_code) {
         this.customer_code = customer_code;
         this.so_prefix = so_prefix;
         this.so_code = so_code;
+        this.file_code = file_code;
     }
 
     @Override
@@ -27,11 +29,12 @@ public class SM_SO_Sql_001 implements Specification {
                 .append(" SELECT\n" +
                         "      S.*\n" +
                         " FROM\n" +
-                        SM_SODao.TABLE + " S\n" +
+                        SM_SO_FileDao.TABLE + " S\n" +
                         " WHERE\n" +
                         "    S.customer_code =  '" + customer_code + "'\n" +
                         "    AND S.so_prefix =  '" + so_prefix + "'\n" +
-                        "    AND S.so_code =    '" + so_code + "' ")
+                        "    AND S.so_code =    '" + so_code + "'\n" +
+                        "    AND S.file_code =  '" + file_code + "' ")
                 .toString();
     }
 }
