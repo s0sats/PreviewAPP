026dcdc5644ca0a42013a6a14d4128cede9a1e3a
DanielLuche <d.luche@namoadigital.com>
2017-08-01 01:02:48 -0300

    Act027  - Add no onResume metodos         mPresenter.getProductInfo();                                    //                                    getSerialInfo();

--- app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
+++ app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
@@ -14,8 +14,8 @@ public class ConstantBaseApp extends ConstantBase {
      * PROJECT ID - PRJ001 - SMS
      */
     public static final String PRJ001_CODE = "PRJ001";
-    public static final String PRJ001_VERSION = "2.0.3";
-    public static final String PKG_CLEAN_APP = "12";
+    public static final String PRJ001_VERSION = "2.0.4";
+    public static final String PKG_CLEAN_APP = "13";
     //          ATUALIZAR ESSA VAR NOS APP BASE PKG_CLEAN
 
     /**
