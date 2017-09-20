fb58b68a29e48435aa80f34c841d9eeff92ba42b
DanielLuche <d.luche@namoadigital.com>
2017-09-20 14:47:35 -0300

    - Modificado Versão o App - Aplicado metodo addUpdateTmp em todos os Dao de Serial.

--- app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
+++ app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
@@ -14,8 +14,8 @@ public class ConstantBaseApp extends ConstantBase {
      * PROJECT ID - PRJ001 - SMS
      */
     public static final String PRJ001_CODE = "PRJ001";
-    public static final String PRJ001_VERSION = "2.1.6";
-    public static final String PKG_CLEAN_APP = "21";
+    public static final String PRJ001_VERSION = "2.1.7";
+    public static final String PKG_CLEAN_APP = "22";
     //          UPDATE ESSA VAR NOS APP BASE PKG_CLEAN
 
     /**
