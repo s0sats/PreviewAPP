4519c54f7c1e9feef0330c57a9a56d47f3748d6b
DanielLuche <d.luche@namoadigital.com>
2017-09-22 17:06:45 -0300

    Versão do app e do banco

--- app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
+++ app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
@@ -14,8 +14,8 @@ public class ConstantBaseApp extends ConstantBase {
      * PROJECT ID - PRJ001 - SMS
      */
     public static final String PRJ001_CODE = "PRJ001";
-    public static final String PRJ001_VERSION = "2.1.7";
-    public static final String PKG_CLEAN_APP = "22";
+    public static final String PRJ001_VERSION = "2.1.8";
+    public static final String PKG_CLEAN_APP = "23";
     //          UPDATE ESSA VAR NOS APP BASE PKG_CLEAN
 
     /**
