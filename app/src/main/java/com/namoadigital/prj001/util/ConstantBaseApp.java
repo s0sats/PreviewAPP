7e1ab1b02b82679369054405e1553948c34ac0ac
DanielLuche <d.luche@namoadigital.com>
2017-10-06 08:47:34 -0300

    Modificado versão do app

--- app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
+++ app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
@@ -14,8 +14,8 @@ public class ConstantBaseApp extends ConstantBase {
      * PROJECT ID - PRJ001 - SMS
      */
     public static final String PRJ001_CODE = "PRJ001";
-    public static final String PRJ001_VERSION = "2.2.1";
-    public static final String PKG_CLEAN_APP = "27";
+    public static final String PRJ001_VERSION = "2.2.2";
+    public static final String PKG_CLEAN_APP = "28";
     //          UPDATE ESSA VAR NOS APP BASE PKG_CLEAN
 
     /**
