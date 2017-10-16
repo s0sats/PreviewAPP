b4e37269131b427f93537f59773b9e6cea7cf127
DanielLuche <d.luche@namoadigital.com>
2017-10-16 14:10:18 -0200

    Merge NH_119 e DL_139

--- app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
+++ app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
@@ -14,8 +14,8 @@ public class ConstantBaseApp extends ConstantBase {
      * PROJECT ID - PRJ001 - SMS
      */
     public static final String PRJ001_CODE = "PRJ001";
-    public static final String PRJ001_VERSION = "2.3.0";
-    public static final String PKG_CLEAN_APP = "29";
+    public static final String PRJ001_VERSION = "2.3.1";
+    public static final String PKG_CLEAN_APP = "30";
     //          UPDATE ESSA VAR NOS APP BASE PKG_CLEAN
 
     /**
