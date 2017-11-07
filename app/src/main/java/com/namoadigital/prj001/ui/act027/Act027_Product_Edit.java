e127fd0f86787e4592cb1f9c100642ee52af96ac
N.Hugo <neonhugo@gmail.com>
2017-11-07 08:51:02 -0200

    Act027_Approval - Inclusao da variavel de traducao approval_quality_lbl

--- app/src/main/java/com/namoadigital/prj001/ui/act027/Act027_Product_Edit.java
+++ app/src/main/java/com/namoadigital/prj001/ui/act027/Act027_Product_Edit.java
@@ -1,11 +1,18 @@
 package com.namoadigital.prj001.ui.act027;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.namoa_digital.namoa_library.ctls.ApplyRepairImageFF;
+import com.namoa_digital.namoa_library.ctls.MKEditTextNM;
+import com.namoa_digital.namoa_library.ctls.PictureFF;
 import com.namoa_digital.namoa_library.view.BaseFragment;
 import com.namoadigital.prj001.R;
 import com.namoadigital.prj001.model.SM_SO;
@@ -15,6 +22,19 @@ import com.namoadigital.prj001.model.SM_SO;
  */
 
 public class Act027_Product_Edit extends BaseFragment{
+
+    private Context context;
+
+    private TextView tv_id_ttl;
+    private TextView tv_desc_ttl;
+    private ApplyRepairImageFF arff_applyrepair;
+    private CheckBox cb_inspection;
+    private MKEditTextNM mk_qtd;
+    private PictureFF pff_sketch;
+    private TextView tv_comments_lbl;
+    private MKEditTextNM mk_comments;
+    private ImageView iv_gallery;
+    private ImageView iv_save;
 
     private SM_SO mSm_so;
     private int mProductCode = -1;
@@ -28,7 +48,6 @@ public class Act027_Product_Edit extends BaseFragment{
         mProductCode = product_code;
         mSeqTmp = seq_tmp;
     }
-
 
     @Nullable
     @Override
@@ -44,9 +63,26 @@ public class Act027_Product_Edit extends BaseFragment{
 
     private void iniVar(View view) {
 
+
+//        sketch = (PictureFF) view.findViewById(R.id.act027_product_edit_sketch);
+//        sketch.setmLabel("Novo Titulo");
+//        sketch.setmValue("1,1");
+//        sketch.setmIv_Dots(0);
+//        sketch.setmV_Line(0);
+//        sketch.setmValue(ToolBox.converterToJson("1,1#2,2#2,5"));
+//
+//        applyRepairImageFF = (ApplyRepairImageFF) view.findViewById(R.id.act027_product_edit_applyrepair);
+//        applyRepairImageFF.setmLabel("Novo Titulo");
+//        applyRepairImageFF.setIv_dots(false);
+//        applyRepairImageFF.setV_Line(false);
+//
+//        applyRepairImageFF.setmIv_Dots(0);
+//        applyRepairImageFF.setmV_Line(0);
+//
+//        applyRepairImageFF.setmValue("01");
+
     }
 
     private void iniAction() {
-
     }
 }
