f9006d0bfbfe3c1cf838b25568500e85d0cfe1c6
DanielLuche <d.luche@namoadigital.com>
2017-12-07 15:38:44 -0200

    CH_MessageDao  - Modificado metodo addUpdate(Iterable), para chamar metodo Toolbox_Inf.chatNextMSGCode() pegando assim o prximo tmp. @#$ WS_C_Message  - Modificado serviço adicionando paramentro de evento  - Caso o evento seja cMessage e o remetente da msg seja o proprio user, atualiza apenas o msg_date @#$ SingletonWebSocket  - No metodo onMessagesReturn, adicionado parametro com o evento chamado no bundle enviado para o serviço  - "Corrigio" log dos evento Reconnect, Reconnecting, Reconnect_error e Reconnect_failed @#$ CH_Message_Sql_004  - Modificado query para quando a tabela ch_messages estiver vazia, os campos prefix e tmp retornarem YYYTMM e 100 @#$ CH_Message_Sql_005  - Seleciona apenas uma msg usando o msg_code como filtro. @#$ Constant  - Adicionado constante CHAT_WS_EVENT_PARAM @#$ Toolbox_Inf  - Adicionado log com o next tmp no metodo chatNextMSGCode

--- app/src/main/java/com/namoadigital/prj001/service_chat/WS_C_Message_Tmp.java
+++ app/src/main/java/com/namoadigital/prj001/service_chat/WS_C_Message_Tmp.java
@@ -18,6 +18,10 @@ import com.namoadigital.prj001.sql.CH_Message_Sql_003;
 import com.namoadigital.prj001.util.Constant;
 import com.namoadigital.prj001.util.ToolBox_Inf;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
 import java.util.ArrayList;
 
 /**
@@ -40,9 +44,9 @@ public class WS_C_Message_Tmp extends IntentService {
         try {
             messageDao = new CH_MessageDao(getApplicationContext());
 
-            String json_param = bundle.getString(Constant.CHAT_WS_JSON_PARAM,null);
+            String json_param = bundle.getString(Constant.CHAT_WS_JSON_PARAM, null);
 
-            if(json_param == null){
+            if (json_param == null) {
                 throw new Exception();
             }
 
@@ -63,7 +67,7 @@ public class WS_C_Message_Tmp extends IntentService {
 
     }
 
-    private void processC_Message_Tmp(String json_param) {
+    private void processC_Message_Tmp(String json_param) throws JSONException {
         Gson gson = new GsonBuilder().serializeNulls().create();
         //
         ArrayList<Chat_C_Message_Tmp> messageTmpList =
@@ -72,9 +76,9 @@ public class WS_C_Message_Tmp extends IntentService {
                         new TypeToken<ArrayList<Chat_C_Message_Tmp>>() {
                         }.getType());
         //
-        if(messageTmpList != null ){
+        if (messageTmpList != null) {
 
-            for (Chat_C_Message_Tmp messageTmp :messageTmpList) {
+            for (Chat_C_Message_Tmp messageTmp : messageTmpList) {
                 CH_Message ch_message =
                         messageDao.getByString(
                                 new CH_Message_Sql_003(
@@ -82,25 +86,54 @@ public class WS_C_Message_Tmp extends IntentService {
                                         messageTmp.getTmp()
                                 ).toSqlQuery()
                         );
-                if(ch_message != null && ch_message.getMsg_prefix() > -1){
-                   ch_message.setMsg_code(messageTmp.getMsg_code());
-                   ch_message.setMsg_pk(String.valueOf(messageTmp.getMsg_prefix() +"_" + ToolBox_Inf.lPad(20,messageTmp.getMsg_code())));
-                   messageDao.addUpdateTmp(ch_message);
+                if (ch_message != null && ch_message.getMsg_prefix() > -1) {
+                    ch_message.setMsg_code(messageTmp.getMsg_code());
+                    ch_message.setMsg_pk(String.valueOf(messageTmp.getMsg_prefix() + "_" + ToolBox_Inf.lPad(20, messageTmp.getMsg_code())));
+                    messageDao.addUpdateTmp(ch_message);
                 }
                 //
                 SingletonWebSocket singletonWebSocket = SingletonWebSocket.getInstance(getApplicationContext());
+
+                JSONObject msg_obj = new JSONObject(ch_message.getMsg_obj());
+                JSONObject msg_obj_content = (JSONObject) msg_obj.get("message");
+                String msg_obj_type = (String) msg_obj_content.get("type");
                 //
-                Chat_S_Message_Tmp sMessageTmp = new Chat_S_Message_Tmp();
-                sMessageTmp.setMsg_prefix(messageTmp.getMsg_prefix());
-                sMessageTmp.setMsg_code(messageTmp.getMsg_code());
-                //
-                singletonWebSocket.attemptSendMessageTmp(
-                        ToolBox_Inf.setWebSocketJsonParam(sMessageTmp)
-                );
+                if (!msg_obj_type.equalsIgnoreCase(Constant.CHAT_MESSAGE_TYPE_IMAGE)) {
+                    //
+                    Chat_S_Message_Tmp sMessageTmp = new Chat_S_Message_Tmp();
+                    sMessageTmp.setMsg_prefix(messageTmp.getMsg_prefix());
+                    sMessageTmp.setMsg_code(messageTmp.getMsg_code());
+                    //
+                    singletonWebSocket.attemptSendMessageTmp(
+                            ToolBox_Inf.setWebSocketJsonParam(sMessageTmp)
+                    );
+                    //Notifica telas
+                    ToolBox_Inf.sendBRChat(getApplicationContext(), Constant.CHAT_BR_TYPE_MSG);
+                } else {
+                    //
+                    String curr_name = ch_message.getMessage_image_local();
+                    String new_name = ch_message.getMsg_prefix() + "." + ch_message.getMsg_code() + ".jpg";
+                    //
+                    renameImage(Constant.CACHE_PATH_PHOTO,curr_name,new_name);
+                    //
+                    curr_name = curr_name.replace(".jpg", "_thumb.jpg");
+                    new_name = curr_name.replace(".jpg", "_thumb.jpg");
+                    //
+                    renameImage(Constant.THU_PATH,curr_name,new_name);
+
+                    // chamar o upload da Imagem com os códigos atualizados
+                    // para incluir a imagem na lista com nome / novo nome iguais
+
+                }
             }
 
         }
-        //Notifica telas
-        ToolBox_Inf.sendBRChat(getApplicationContext(),Constant.CHAT_BR_TYPE_MSG);
+    }
+
+    private boolean renameImage(String path, String curr_name, String new_name) {
+        File from = new File(path + "/", curr_name);
+        File to = new File(path + "/", new_name);
+        //
+        return from.renameTo(to);
     }
 }
