b0a073c124e2983d0b404a1386094df7220e7e7e
DanielLuche <d.luche@namoadigital.com>
2018-02-02 17:03:58 -0200

    SingletonWebSocket  - Modificado cError e cErrorLogin para, quando CHAT_SESSION_NOT_FOUND força novo login, se SESSION_NOT_FOUND, para serviço @#$ Act034 e Act035  - Removido item de menu com icone de conexão

--- app/src/main/java/com/namoadigital/prj001/ui/act034/Act034_Main.java
+++ app/src/main/java/com/namoadigital/prj001/ui/act034/Act034_Main.java
@@ -924,29 +924,29 @@ public class Act034_Main extends Base_Activity_Frag implements Act034_Main_View 
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         menu.add(0, 1, Menu.NONE, getResources().getString(R.string.app_name));
-        menu.add(0, 2, Menu.NONE + 1, getResources().getString(R.string.app_name));
+        //menu.add(0, 2, Menu.NONE + 1, getResources().getString(R.string.app_name));
 
         menu.getItem(0).setIcon(getResources().getDrawable(R.mipmap.ic_namoa));
         menu.getItem(0).setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS);
-        boolean logged = false;
-        if(SingletonWebSocket.isSingletonWebSocketSetted()) {
-            SingletonWebSocket singletonWebSocket = SingletonWebSocket.getInstance(context);
-            logged = singletonWebSocket.ismSocketLogged();
-        }
-
-        if(logged && AppBackgroundService.isRunning){
-            menu.getItem(1).setIcon(R.drawable.ic_swap_vertical_circle_green_24dp);
-            menu.getItem(1).setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS);
-        } else if(logged && !AppBackgroundService.isRunning) {
-            menu.getItem(1).setIcon(R.drawable.ic_swap_vertical_circle_black_24dp);
-            menu.getItem(1).setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS);
-        }else if(!logged && AppBackgroundService.isRunning) {
-            menu.getItem(1).setIcon(R.drawable.ic_swap_vertical_circle_yellow_24dp);
-            menu.getItem(1).setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS);
-        }else{
-            menu.getItem(1).setIcon(R.drawable.ic_swap_vertical_circle_red_24dp);
-            menu.getItem(1).setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS);
-        }
+//        boolean logged = false;
+//        if(SingletonWebSocket.isSingletonWebSocketSetted()) {
+//            SingletonWebSocket singletonWebSocket = SingletonWebSocket.getInstance(context);
+//            logged = singletonWebSocket.ismSocketLogged();
+//        }
+//
+//        if(logged && AppBackgroundService.isRunning){
+//            menu.getItem(1).setIcon(R.drawable.ic_swap_vertical_circle_green_24dp);
+//            menu.getItem(1).setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS);
+//        } else if(logged && !AppBackgroundService.isRunning) {
+//            menu.getItem(1).setIcon(R.drawable.ic_swap_vertical_circle_black_24dp);
+//            menu.getItem(1).setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS);
+//        }else if(!logged && AppBackgroundService.isRunning) {
+//            menu.getItem(1).setIcon(R.drawable.ic_swap_vertical_circle_yellow_24dp);
+//            menu.getItem(1).setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS);
+//        }else{
+//            menu.getItem(1).setIcon(R.drawable.ic_swap_vertical_circle_red_24dp);
+//            menu.getItem(1).setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS);
+//        }
 
         return true;
     }
