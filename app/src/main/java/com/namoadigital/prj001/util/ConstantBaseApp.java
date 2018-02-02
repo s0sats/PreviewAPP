aee534e141dd428b0ec864c34ea6b391a8b767a3
DanielLuche <d.luche@namoadigital.com>
2018-02-02 17:14:44 -0200

    Modificado versão do app, merge entre dl_204 e nh179

--- app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
+++ app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
@@ -14,8 +14,8 @@ public class ConstantBaseApp extends ConstantBase {
      * PROJECT ID - PRJ001 - SMS
      */
     public static final String PRJ001_CODE = "PRJ001";
-    public static final String PRJ001_VERSION = "2.6.2.5";
-    public static final String PKG_CLEAN_APP = "56";
+    public static final String PRJ001_VERSION = "2.6.2.6";
+    public static final String PKG_CLEAN_APP = "57";
     //          UPDATE ESSA VAR NOS APP BASE PKG_CLEAN
 
     /**
