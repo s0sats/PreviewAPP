54c300942d019c1ee8748abc517d32c3d49cf556
DanielLuche <d.luche@namoadigital.com>
2018-03-05 14:35:46 -0300

    AppBase  - Modificado versão do Banco do customer @#$ Constant  - Trocado versão do app para 2.6.2.12

--- app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
+++ app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
@@ -14,8 +14,8 @@ public class ConstantBaseApp extends ConstantBase {
      * PROJECT ID - PRJ001 - SMS
      */
     public static final String PRJ001_CODE = "PRJ001";
-    public static final String PRJ001_VERSION = "2.6.2.10";
-    public static final String PKG_CLEAN_APP = "61";
+    public static final String PRJ001_VERSION = "2.6.2.12";
+    public static final String PKG_CLEAN_APP = "62";
     //          UPDATE ESSA VAR NOS APP BASE PKG_CLEAN
 
     /**
