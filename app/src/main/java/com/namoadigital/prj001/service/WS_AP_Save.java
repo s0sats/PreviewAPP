41c7176c1003bc2b51b6feb859a5eadf5128d31e
DanielLuche <d.luche@namoadigital.com>
2018-03-09 15:45:46 -0300

    GE_Custom_Form_ApDao  - Adicionado registro de exception no metodo .remove() @#$ WS_AP_Save  - Removido constant AP_RETURN_LIST que não era usada  - No processamento do Retorno do save, adicionado metodo checkForApExclusion(), que analisa a necessidade a apagar o AP salvo. Caso haja necessidade, apaga o registro localmente e adiciona msg informando que ap foi apagado  - Criado metodo checkForApExclusion() que analisa se o usr é o quem ou tem aquela a room no chat, caso não possui nenhum dos vinculos, apaga regitro.  - Modificado metodo getFormApInfo() para retornar o obj GE_Custom_Form_Ap. @#$ Act016 e Act017  - Modificado telas usarem o mesmo layout no dialog de filtro por modulo @#$ REs   - Renomeado o arquivo act016_filter_dialog para module_filter_dialog

--- app/src/main/java/com/namoadigital/prj001/service/WS_AP_Save.java
+++ app/src/main/java/com/namoadigital/prj001/service/WS_AP_Save.java
@@ -10,13 +10,17 @@ import com.google.gson.GsonBuilder;
 import com.namoa_digital.namoa_library.util.HMAux;
 import com.namoa_digital.namoa_library.util.ToolBox;
 import com.namoadigital.prj001.R;
+import com.namoadigital.prj001.dao.CH_RoomDao;
 import com.namoadigital.prj001.dao.GE_Custom_Form_ApDao;
+import com.namoadigital.prj001.model.CH_Room;
 import com.namoadigital.prj001.model.GE_Custom_Form_Ap;
 import com.namoadigital.prj001.model.TSave_Ap_Env;
 import com.namoadigital.prj001.model.TSave_Ap_Rec;
 import com.namoadigital.prj001.receiver.WBR_AP_Save;
+import com.namoadigital.prj001.sql.CH_Room_Sql_001;
 import com.namoadigital.prj001.sql.GE_Custom_Form_Ap_Sql_006;
 import com.namoadigital.prj001.sql.GE_Custom_Form_Ap_Sql_009;
+import com.namoadigital.prj001.sql.GE_Custom_Form_Ap_Sql_010;
 import com.namoadigital.prj001.util.Constant;
 import com.namoadigital.prj001.util.ToolBox_Con;
 import com.namoadigital.prj001.util.ToolBox_Inf;
@@ -29,7 +33,6 @@ import java.util.List;
  */
 
 public class WS_AP_Save extends IntentService {
-    public static final String AP_RETURN_LIST = "AP_RETURN_LIST";
 
     private HMAux hmAux_Trans = new HMAux();
     private String mModule_Code = Constant.APP_MODULE;
@@ -126,49 +129,65 @@ public class WS_AP_Save extends IntentService {
             return;
         }
         //
-        processApSaveReturn(rec,apList);
+        processApSaveReturn(rec, apList);
     }
 
     private void processApSaveReturn(TSave_Ap_Rec rec, ArrayList<GE_Custom_Form_Ap> apList) {
 
-        switch (rec.getSave()){
+        switch (rec.getSave()) {
             case "OK":
                 HMAux auxApReturned = new HMAux();
                 //
-                for (TSave_Ap_Rec.ApSaveStatus apSaveStatus: rec.getAp()) {
+                for (TSave_Ap_Rec.ApSaveStatus apSaveStatus : rec.getAp()) {
                     String hmAuxPKKey =
-                            apSaveStatus.getCustomer_code()+"."+
-                            apSaveStatus.getCustom_form_type()+"."+
-                            apSaveStatus.getCustom_form_code()+"."+
-                            apSaveStatus.getCustom_form_version()+"."+
-                            apSaveStatus.getCustom_form_data()+"."+
-                            apSaveStatus.getAp_code();
+                            apSaveStatus.getCustomer_code() + "." +
+                                    apSaveStatus.getCustom_form_type() + "." +
+                                    apSaveStatus.getCustom_form_code() + "." +
+                                    apSaveStatus.getCustom_form_version() + "." +
+                                    apSaveStatus.getCustom_form_data() + "." +
+                                    apSaveStatus.getAp_code();
 
-                    String ap_desc = getFormApInfo(apSaveStatus,apList);
+                    GE_Custom_Form_Ap formAp = getFormApInfo(apSaveStatus, apList);
+                    //
+                    String ap_desc = formAp != null ? formAp.getAp_code() + " - " + formAp.getAp_description() : "";
+                    //StatusCode é o codigo do status do server
+                    //1 = OK
+                    boolean wasApDeleted = false;
+                    if (apSaveStatus.getStatus_code() == 1) {
+                        formApDao.addUpdate(
+                                new GE_Custom_Form_Ap_Sql_009(
+                                        apSaveStatus.getCustomer_code(),
+                                        apSaveStatus.getCustom_form_type(),
+                                        apSaveStatus.getCustom_form_code(),
+                                        apSaveStatus.getCustom_form_version(),
+                                        apSaveStatus.getCustom_form_data(),
+                                        apSaveStatus.getAp_code(),
+                                        apSaveStatus.getAp_scn()
+                                ).toSqlQuery()
+                        );
+                        //
+                       wasApDeleted = checkForApExclusion(formAp);
+                    }
+                    //Define msg do HmAux
+                    String status = "";
+                    if(apSaveStatus.getStatus_code() == 1){
+                        if(wasApDeleted) {
+                            status = hmAux_Trans.get("msg_ap_removed");
+                        }else{
+                            status = String.valueOf(apSaveStatus.getStatus_code());
+                        }
+                    }else{
+                        status = apSaveStatus.getStatus_msg();
+                    }
                     //
                     auxApReturned.put(
                             hmAuxPKKey,
                             (ap_desc.length() > 0 ? (ap_desc + Constant.MAIN_CONCAT_STRING) : "") +
-                            (apSaveStatus.getStatus_code() == 1 ? String.valueOf(apSaveStatus.getStatus_code()) : apSaveStatus.getStatus_msg())
+                            status
                     );
-                    //StatusCode é o codigo do status do server
-                    //1 = OK
-                    if(apSaveStatus.getStatus_code() == 1 ){
-                        formApDao.addUpdate(
-                            new GE_Custom_Form_Ap_Sql_009(
-                                    apSaveStatus.getCustomer_code(),
-                                    apSaveStatus.getCustom_form_type(),
-                                    apSaveStatus.getCustom_form_code(),
-                                    apSaveStatus.getCustom_form_version(),
-                                    apSaveStatus.getCustom_form_data(),
-                                    apSaveStatus.getAp_code(),
-                                    apSaveStatus.getAp_scn()
-                            ).toSqlQuery()
-                        );
-                    }
                 }
                 //
-                ToolBox.sendBCStatus(getApplicationContext(), "CLOSE_ACT", hmAux_Trans.get("msg_end_ap_save"),auxApReturned , "", "0");
+                ToolBox.sendBCStatus(getApplicationContext(), "CLOSE_ACT", hmAux_Trans.get("msg_end_ap_save"), auxApReturned, "", "0");
                 break;
             case "ERROR":
                 ToolBox_Inf.sendBCStatus(getApplicationContext(), "ERROR_1", rec.getError_msg(), "", "0");
@@ -178,31 +197,76 @@ public class WS_AP_Save extends IntentService {
 
     }
 
-    private String getFormApInfo(TSave_Ap_Rec.ApSaveStatus apSaveStatus, ArrayList<GE_Custom_Form_Ap> apList) {
-        for (GE_Custom_Form_Ap formAp : apList) {
-            if(
-                apSaveStatus.getCustomer_code() == formAp.getCustomer_code() &&
-                apSaveStatus.getCustom_form_type() == formAp.getCustom_form_type() &&
-                apSaveStatus.getCustom_form_code() == formAp.getCustom_form_code() &&
-                apSaveStatus.getCustom_form_version() == formAp.getCustom_form_version() &&
-                apSaveStatus.getCustom_form_data() == formAp.getCustom_form_data() &&
-                apSaveStatus.getAp_code() == formAp.getAp_code()
-            ){
-                return formAp.getAp_code() + " - " + formAp.getAp_description();
+    /**
+     * Verifica se precisa apagar o AP
+     * @param formAp
+     */
+    private boolean checkForApExclusion(GE_Custom_Form_Ap formAp) {
+        int user_code = ToolBox_Inf.convertStringToInt(ToolBox_Con.getPreference_User_Code(getApplicationContext()));
+        boolean deleteAP = false;
+        //
+        if(formAp.getAp_who() != user_code){
+            if(formAp.getRoom_code() == null){
+                deleteAP = true;
+            }else{
+                CH_RoomDao roomDao = new CH_RoomDao(getApplicationContext());
+                //
+                CH_Room chRoom = roomDao.getByString(
+                        new CH_Room_Sql_001(
+                                formAp.getRoom_code()
+                        ).toSqlQuery()
+                );
+                //
+                if(chRoom == null){
+                    deleteAP = true;
+                }
             }
         }
         //
-        return "";
+        if(deleteAP){
+            formApDao.remove(
+                    new GE_Custom_Form_Ap_Sql_010(
+                            formAp.getCustomer_code(),
+                            formAp.getCustom_form_type(),
+                            formAp.getCustom_form_code(),
+                            formAp.getCustom_form_version(),
+                            formAp.getCustom_form_data(),
+                            formAp.getAp_code()
+                    ).toSqlQuery()
+            );
+            //
+            return true;
+        }
+        //
+        return false;
+    }
+
+    private GE_Custom_Form_Ap getFormApInfo(TSave_Ap_Rec.ApSaveStatus apSaveStatus, ArrayList<GE_Custom_Form_Ap> apList) {
+        for (GE_Custom_Form_Ap formAp : apList) {
+            if (
+                    apSaveStatus.getCustomer_code() == formAp.getCustomer_code() &&
+                            apSaveStatus.getCustom_form_type() == formAp.getCustom_form_type() &&
+                            apSaveStatus.getCustom_form_code() == formAp.getCustom_form_code() &&
+                            apSaveStatus.getCustom_form_version() == formAp.getCustom_form_version() &&
+                            apSaveStatus.getCustom_form_data() == formAp.getCustom_form_data() &&
+                            apSaveStatus.getAp_code() == formAp.getAp_code()
+                    ) {
+                return formAp;
+            }
+        }
+        //
+        return null;
     }
 
 
     private void loadTranslation() {
         List<String> translist = new ArrayList<>();
-
+        //
         translist.add("msg_no_ap_to_save");
         translist.add("msg_sending_ap_data");
         translist.add("msg_processing_data_returned");
         translist.add("msg_end_ap_save");
+        translist.add("msg_ap_removed");
         //
         mResource_Code = ToolBox_Inf.getResourceCode(
                 getApplicationContext(),
