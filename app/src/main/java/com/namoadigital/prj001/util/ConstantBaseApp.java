3c4c40bced4a086e81fb7d11e432b60a34482b35
DanielLuche <d.luche@namoadigital.com>
2018-04-12 17:01:54 -0300

    Correções do Chat loop infinito em custoemr que não tinham chat Mudança de versão para 3.2.1.4

--- app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
+++ app/src/main/java/com/namoadigital/prj001/util/ConstantBaseApp.java
@@ -14,8 +14,8 @@ public class ConstantBaseApp extends ConstantBase {
      * PROJECT ID - PRJ001 - SMS
      */
     public static final String PRJ001_CODE = "PRJ001";
-    public static final String PRJ001_VERSION = "3.2.1.3";
-    public static final String PKG_CLEAN_APP = "80";
+    public static final String PRJ001_VERSION = "3.2.1.4";
+    public static final String PKG_CLEAN_APP = "81";
 
     public static String PRJ001_VERSION_NEW = "";
     public static int PRJ001_VERSION_NUMBER = -1;
