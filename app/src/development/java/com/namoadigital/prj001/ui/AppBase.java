c1b1947976f83f8f1ca3a12022c08a6c04cabf56
DanielLuche <d.luche@namoadigital.com>
2018-08-22 10:37:57 -0300

    Atualizado versão do App e dos banco mult

--- app/src/development/java/com/namoadigital/prj001/ui/AppBase.java
+++ app/src/development/java/com/namoadigital/prj001/ui/AppBase.java
@@ -87,7 +87,7 @@ public class AppBase extends Application {
         DB_FULL_BASE = DB_PATH + "/" + DB_NAME_BASE;
 
         DB_NAME_CUSTOM = "cc.db3";
-        DB_VERSION_CUSTOM = 29;
+        DB_VERSION_CUSTOM = 30;
         DB_FULL_CUSTOM = DB_PATH + "/" + DB_NAME_CUSTOM;
 
         //DB_NAME_CHAT = "namoa_chat.db3";
