e19ea13d1275340097fcf491f20077575cc0f145
DanielLuche <d.luche@namoadigital.com>
2019-01-21 17:08:49 -0200

    Main_header_Env  - Criado modelo "Pai" de envio contento as propriedades que sempre são enviadas ao server: app_code, app_version e app_ocr_vin @#$ TGC_Env  - Add herança da classe Main_header_Env  - Comentado as propriedades que foram movidas para o obj pai. @#$ WS_GetCustomer  - Add o set da propriedade app_orc_vin na classe de envio da chamada do server

---
+++ app/src/main/java/com/namoadigital/prj001/model/Main_Header_Env.java
@@ -0,0 +1,32 @@
+package com.namoadigital.prj001.model;
+
+public class Main_Header_Env {
+
+    private String app_code;
+    private String app_version;
+    private int app_ocr_vin;
+
+    public String getApp_code() {
+        return app_code;
+    }
+
+    public void setApp_code(String app_code) {
+        this.app_code = app_code;
+    }
+
+    public String getApp_version() {
+        return app_version;
+    }
+
+    public void setApp_version(String app_version) {
+        this.app_version = app_version;
+    }
+
+    public int getApp_ocr_vin() {
+        return app_ocr_vin;
+    }
+
+    public void setApp_ocr_vin(int app_ocr_vin) {
+        this.app_ocr_vin = app_ocr_vin;
+    }
+}
--- app/src/main/java/com/namoadigital/prj001/service/WS_GetCustomer.java
+++ app/src/main/java/com/namoadigital/prj001/service/WS_GetCustomer.java
@@ -91,6 +91,7 @@ public class WS_GetCustomer extends IntentService {
         env.setApp_code(Constant.PRJ001_CODE);
         env.setApp_version(Constant.PRJ001_VERSION);
         env.setDevice_code(ToolBox_Inf.uniqueID(getApplicationContext()));
+        env.setApp_ocr_vin(ToolBox_Inf.isMicroBlinkImported() ? 1 : 0 );
         //
         env.setEmail_p(user);
         env.setPassword(password);
