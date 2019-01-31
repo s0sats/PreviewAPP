4ea57412473f3bdc926acaa95cf2c4e26ef0c90b
DanielLuche <d.luche@namoadigital.com>
2019-01-31 16:06:37 -0200

    Merge DL_349 x LB017  Renomeado fragmentos @#$ Act050_Frag_Parameters  - Add Serial  - Criado metodos  onContractSelected, onMoveToOSFragment onBackButtonClick na interface OnFragParameterInteraction  - Add Scroll para o fim do scrollview ao selecionar um contrato. @#$ Act050  - Criado var do novo fragment  - Implementado metodo onMoveToOSFragment() da interface do frg_parameters  - Add metodo para inicializar a SO e preenchimento do Favorito ao clicar no item da lista.  - Implementado chamada do frg_parameters no clique do item de favorito.

--- app/src/main/java/com/namoadigital/prj001/ui/act050/Act050_Frag_SO.java
+++ app/src/main/java/com/namoadigital/prj001/ui/act050/Act050_Frag_SO.java
@@ -66,7 +66,7 @@ public class Act050_Frag_SO extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.act050_s0_creation_fragment, container, false);
+        return inflater.inflate(R.layout.act050_frag_so, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
