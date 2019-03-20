638c77dd36af24ad6cda690b3edbeccb0e121c95
LBarrionuevo <45567294+LBarrionuevo@users.noreply.github.com>
2019-03-20 11:16:53 -0300

    Layout Pesquisa Ordem de Movimentação

---
+++ app/src/main/java/com/namoadigital/prj001/ui/act053/Act053_Main.java
@@ -0,0 +1,15 @@
+package com.namoadigital.prj001.ui.act053;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.namoadigital.prj001.R;
+
+public class Act053_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act053_main);
+    }
+}
