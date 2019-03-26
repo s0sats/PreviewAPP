0f854998a1ebead9e005be984bd0556ebf65ffe1
LBarrionuevo <45567294+LBarrionuevo@users.noreply.github.com>
2019-03-26 16:43:53 -0300

    Adapter da lista de ordem de movimentacao

--- app/src/main/java/com/namoadigital/prj001/ui/act055/Act055_Main.java
+++ app/src/main/java/com/namoadigital/prj001/ui/act055/Act055_Main.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.namoadigital.prj001.R;
+import com.namoadigital.prj001.adapter.Act055IOMoveOrderListAdapter;
 
-public class Act055_Main extends AppCompatActivity {
+public class Act055_Main extends AppCompatActivity  implements Act055IOMoveOrderListAdapter.Act055ListListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act055_main);
     }
+
 }
