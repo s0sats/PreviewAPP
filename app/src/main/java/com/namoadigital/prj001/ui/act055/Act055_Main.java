69c9929c25425a429036740c8db29954c30393fa
DanielLuche <d.luche@namoadigital.com>
2019-03-26 17:09:27 -0300

    MERge DL_380 + LB_053 Act055  - Implementado interface

--- app/src/main/java/com/namoadigital/prj001/ui/act055/Act055_Main.java
+++ app/src/main/java/com/namoadigital/prj001/ui/act055/Act055_Main.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import com.namoadigital.prj001.R;
 import com.namoadigital.prj001.adapter.Act055IOMoveOrderListAdapter;
+import com.namoadigital.prj001.model.IO_Move_Search_Record;
 
 public class Act055_Main extends AppCompatActivity  implements Act055IOMoveOrderListAdapter.Act055ListListener {
 
@@ -14,4 +15,13 @@ public class Act055_Main extends AppCompatActivity  implements Act055IOMoveOrder
         setContentView(R.layout.act055_main);
     }
 
+    @Override
+    public void showAlertSerialOut(String alert_serial_out_site_title, String alert_serial_out_site_msg) {
+
+    }
+
+    @Override
+    public void onClickListItem(IO_Move_Search_Record record) {
+
+    }
 }
