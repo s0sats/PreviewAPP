5defe17a81d9d26d9025b4feb7f0733d2f42af22
LBarrionuevo <45567294+LBarrionuevo@users.noreply.github.com>
2019-04-08 16:11:13 -0300

    models de save

---
+++ app/src/main/java/com/namoadigital/prj001/model/T_IO_Move_Save_Rec.java
@@ -0,0 +1,10 @@
+package com.namoadigital.prj001.model;
+
+import java.util.ArrayList;
+
+public class T_IO_Move_Save_Rec {
+    private String app;
+    private String validation;
+    private String save;
+    private ArrayList<IO_Move_Return> record;
+}
