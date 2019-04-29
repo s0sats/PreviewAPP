bd192ac19b84944184457b4b0996dfb13c51a487
DanielLuche <d.luche@namoadigital.com>
2019-04-29 14:49:35 -0300

    Correção trad drawer inbound

--- app/src/development/java/com/namoadigital/prj001/util/Constant.java
+++ app/src/development/java/com/namoadigital/prj001/util/Constant.java
@@ -51,12 +51,15 @@ public class Constant extends ConstantBaseApp {
     public static final String WS_IO_INBOUND_SEARCH = "https://dev.namoadigital.com/ws/prj001/io/server_inbound.ws";
     public static final String WS_IO_OUTBOUND_SEARCH = "https://dev.namoadigital.com/ws/prj001/io/server_outbound.ws";
     public static final String WS_IO_MOVE_DOWNLOAD = "https://dev.namoadigital.com/ws/prj001/io/server_move_obj.ws";
+    public static final String WS_IO_MOVE_SAVE = "https://dev.namoadigital.com/ws/prj001/io/server_move_save.ws";
     public static final String WS_IO_INBOUND_DOWNLOAD = "https://dev.namoadigital.com/ws/prj001/io/server_inbound_obj.ws";
     public static final String WS_IO_OUTBOUND_DOWNLOAD = "https://dev.namoadigital.com/ws/prj001/io/server_outbound_obj.ws";
     public static final String WS_IO_MASTER_DATA = "https://dev.namoadigital.com/ws/prj001/io/server_master_data.ws";
     public static final String WS_IO_FROM_OUTBOUND_SEARCH = "https://dev.namoadigital.com/ws/prj001/io/server_outbound_search.ws";
     public static final String WS_IO_INBOUND_HEADER_SAVE = "https://dev.namoadigital.com/ws/prj001/io/server_inbound_save.ws";
     public static final String WS_IO_ADDRESS_SUGGESTION = "https://dev.namoadigital.com/ws/prj001/io/server_address_suggestion.ws";
+    public static final String WS_IO_BLIND_SAVE = "https://dev.namoadigital.com/ws/prj001/io/server_blind_save.ws";
+    public static final String WS_IO_INBOUND_ITEM_SAVE = "https://dev.namoadigital.com/ws/prj001/io/server_inbound_item_save.ws";
 
 
     public static final String WS_ENABLE_NFC = "https://dev.namoadigital.com/ws/prj001/server_enable_nfc_auth.ws";
