7dde3003c8de227600e8f9463fc3e455d08e3dfd
LBarrionuevo <45567294+LBarrionuevo@users.noreply.github.com>
2019-06-03 09:36:08 -0300

    Layout de tela BlindMove e refatoração pra nomes genericos.

---
+++ app/src/main/java/com/namoadigital/prj001/ui/act064/Act064_Main_Contract.java
@@ -0,0 +1,14 @@
+package com.namoadigital.prj001.ui.act064;
+
+public interface Act064_Main_Contract {
+
+    interface I_View{
+
+
+    }
+
+    interface I_Presenter{
+
+
+    }
+}
---
+++ app/src/main/java/com/namoadigital/prj001/ui/act064/Act064_Main_Presenter.java
@@ -0,0 +1,26 @@
+package com.namoadigital.prj001.ui.act064;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.namoa_digital.namoa_library.util.HMAux;
+import com.namoa_digital.namoa_library.util.ToolBox;
+import com.namoadigital.prj001.dao.MD_ProductDao;
+import com.namoadigital.prj001.dao.MD_Product_SerialDao;
+import com.namoadigital.prj001.model.MD_Product;
+import com.namoadigital.prj001.model.MD_Product_Serial;
+import com.namoadigital.prj001.sql.MD_Product_Sql_003;
+import com.namoadigital.prj001.util.Constant;
+import com.namoadigital.prj001.util.ToolBox_Con;
+
+import java.util.ArrayList;
+
+public class Act064_Main_Presenter implements Act064_Main_Contract.I_Presenter {
+
+    private Context context;
+    private Act064_Main_Contract.I_View mView;
+    private HMAux hmAux_Trans = new HMAux();
+
+
+
+}
