fa96ac3db7f7f6b8c2fe6750cfd2e28eb85023f1
LBarrionuevo <l.barrionuevo@namoadigital.com>
2019-12-30 11:40:01 -0300

    Insercao de comentario sobre a branch @#$

--- app/src/main/java/com/namoadigital/prj001/ui/act001/Act001_Main_Presenter_Impl.java
+++ app/src/main/java/com/namoadigital/prj001/ui/act001/Act001_Main_Presenter_Impl.java
@@ -66,6 +66,10 @@ public class Act001_Main_Presenter_Impl implements Act001_Main_Presenter {
         if(ToolBox_Con.isOnline(context)){
             mView.showPD();
             //
+            /*
+                Barrionuevo - 30.12.2019
+                Tratamento para pular verificacao de versao caso haja dados pedentes
+             */
             int status_jump = 0;
             if(ToolBox_Inf.hasPendingData(context,ToolBox_Inf.getListDB("C_", true))){
                 status_jump = 1;
