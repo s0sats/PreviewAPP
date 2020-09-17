a1e3aaf8e5266867df631dfb767989a30d728283
DanielLuche <d.luche@namoadigital.com>
2020-09-17 12:23:56 -0300

    Sql_WS_TK_Ticket_Save_001  - Modificado query para não selecionar tickest que tenham form compendencia de gps

--- app/src/main/java/com/namoadigital/prj001/sql/Sql_WS_TK_Ticket_Save_001.java
+++ app/src/main/java/com/namoadigital/prj001/sql/Sql_WS_TK_Ticket_Save_001.java
@@ -1,13 +1,17 @@
 package com.namoadigital.prj001.sql;
 
+import com.namoadigital.prj001.dao.GE_Custom_Form_DataDao;
 import com.namoadigital.prj001.dao.TK_TicketDao;
 import com.namoadigital.prj001.dao.TK_Ticket_CtrlDao;
 import com.namoadigital.prj001.dao.TK_Ticket_StepDao;
 import com.namoadigital.prj001.database.Specification;
+import com.namoadigital.prj001.util.ConstantBaseApp;
 
 /**
  * LUCHE - 30/07/2020
  * Seleciona todos os ticket que tenham update required no ticket, step, product ou ctrls.
+ * LUCHE - 17/09/2020
+ * Modificado query para não selecionar tickest que tenham form compendencia de gps
  */
 
 public class Sql_WS_TK_Ticket_Save_001 implements Specification {
@@ -46,6 +50,14 @@ public class Sql_WS_TK_Ticket_Save_001 implements Specification {
                     "            OR s.update_required = 1\n" +
                     "            OR c.update_required = 1\n" +
                     "           )\n" +
+                    "      and not exists( SELECT 1\n" +
+                    "                      FROM "+ GE_Custom_Form_DataDao.TABLE +" g\n" +
+                    "                      WHERE g.customer_code = t.current_step_order\n" +
+                    "                            and g.ticket_prefix = t.ticket_prefix\n" +
+                    "                            and g.ticket_code = t.ticket_code  \n" +
+                    "                            and g.custom_form_status = '"+ ConstantBaseApp.SYS_STATUS_WAITING_SYNC +"'\n" +
+                    "                            and g.location_pendency = 1  \n" +
+                    "                     )  " +
                     " GROUP BY\n" +
                     "     t.customer_code,\n" +
                     "     t.ticket_prefix,\n" +
