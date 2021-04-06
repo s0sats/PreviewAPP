64ab39a8f74f079af7c4d7b66875f7e13fd9ae94
DanielLuche <d.luche@namoadigital.com>
2021-04-06 09:41:05 -0300

    PROJECT build.gradle  - Modificado versão do kotlin para versão estavel. @#$ MyActionsAdapter.kt - EM DEV  - Criado adapter para a lista de actions / process @#$ MyActions  - Criado data class que será usado como obj do adapter @#$ Act083_Main  - Instanciado View model  - No layout, criado id para o include do layout interno para set acessado pelo viewBinding @#$ Act083ViewModel  - Criado view model - AINDA VAZIO @#$ App.build.gradle  - Movido os imports do kotlin para cima e adicionado valor fixo para versão.  - Add dependencia do lifecycle-extensions

--- app/src/main/java/com/namoadigital/prj001/adapter/MyActionsAdapter.kt
+++ app/src/main/java/com/namoadigital/prj001/adapter/MyActionsAdapter.kt
@@ -1,9 +1,11 @@
 package com.namoadigital.prj001.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.namoadigital.prj001.databinding.MyActionsItemBinding
@@ -11,11 +13,10 @@ import com.namoadigital.prj001.model.MyActions
 
 class MyActionsAdapter(
         private val myActions: List<MyActions>
-        ) : RecyclerView.Adapter<MyActionsAdapter.MyActionVh>()
-{
+) : RecyclerView.Adapter<MyActionsAdapter.MyActionVh>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyActionVh {
-        return MyActionVh(MyActionsItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+        return MyActionVh(MyActionsItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun onBindViewHolder(holder: MyActionVh, position: Int) {
@@ -26,30 +27,50 @@ class MyActionsAdapter(
         return myActions.size
     }
 
-    inner class MyActionVh(private val binding: MyActionsItemBinding) : RecyclerView.ViewHolder(binding.root){
-        private val context by lazy {
-            binding.root.context
-        }
+    inner class MyActionVh(private val binding: MyActionsItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        fun onBinding(myAction: MyActions){
-               binding.myActionsItemTvCode.text = myAction.processId
-               binding.myActionsItemTvStatus.text = myAction.processStatus
-               binding.myActionsItemTvPlannedDate.text = myAction.plannedDate
-               //
-               myAction.processLeftIcon.let{ leftIcon ->
-                   binding.myActionsItemIvIconLeft.apply {
-                       setImageDrawable(
-                               leftIcon?.let {
-                                   it -> ContextCompat.getDrawable(context, it)
-                               }
-                       )
-                       //
-                       visibility = View.VISIBLE
-                   }
-               }
-               //
-               binding.myActionsItemIvIconRight.setImageDrawable(ContextCompat.getDrawable(binding.root.context, myAction.processRightIcon))
-           }
+        fun onBinding(myAction: MyActions) {
+            binding.myActionsItemTvCode.text = myAction.processId
+            binding.myActionsItemTvStatus.text = myAction.processStatus
+            binding.myActionsItemTvPlannedDate.text = myAction.plannedDate
+            //
+            binding.myActionsItemIvIconLeft.applyVisibilityIfSourceExists(myAction.processLeftIcon)
+            binding.myActionsItemIvIconRight.applyVisibilityIfSourceExists(myAction.processRightIcon)
+            //
+            binding.myActionsItemTvProdDesc.text = myAction.productDesc
+            binding.myActionsItemTvSerialId.text = myAction.serialId
+            binding.myActionsItemTvProcessDesc.text = myAction.processDesc
+            binding.myActionsItemTvFocusStepDesc.text = myAction.focusStepDesc
+            //
+            binding.myActionsItemTvSite.applyVisibilityIfTextExists(myAction.siteDesc)
+            binding.myActionsItemTvClient.applyVisibilityIfTextExists(myAction.clientInfo)
+            binding.myActionsItemTvContract.applyVisibilityIfTextExists(myAction.contractInfo)
+            binding.myActionsItemTvContract.applyVisibilityIfTextExists(myAction.contractInfo)
+            binding.myActionsItemTvOsCode.applyVisibilityIfTextExists(myAction.serviceOrderCode)
+            binding.myActionsItemTvDoneDate.applyVisibilityIfTextExists(myAction.doneDate)
+        }
     }
 
+    /**
+     * Kotlin extension para TextView e filhos para setar visibilidade apenas se o text existir
+     */
+    fun TextView.applyVisibilityIfTextExists(text: String?){
+        this.visibility = if (!text.isNullOrEmpty()){
+            View.VISIBLE
+        }else{
+            View.GONE
+        }
+    }
+
+    /**
+     * Kotlin extension para ImageView que seta o source e visibilidade caso exista.
+     */
+    fun ImageView.applyVisibilityIfSourceExists(@DrawableRes source: Int?){
+        this.visibility = if (source != null){
+            setImageDrawable(ContextCompat.getDrawable(this.context, source))
+            View.VISIBLE
+        }else{
+            View.GONE
+        }
+    }
 }
