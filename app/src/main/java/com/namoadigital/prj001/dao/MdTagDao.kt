1bef9eddac7ef8ab4f43f4545ef2bf8f575cd24f
Daniel Luche <d.luche@namoadigital.com>
2021-04-19 17:10:15 -0300

    MdTag, MdTagDao e DatabaseHelperMulti  - Criado modelo, dao e script de banco da tabela de tema. @#$ WS_Sync  - Add processamento do arquivo de temas junto com o package main @#$ MdTagSqlTruncate.kt  - Criado query truncate para a nova table.

---
+++ app/src/main/java/com/namoadigital/prj001/dao/MdTagDao.kt
@@ -0,0 +1,296 @@
+package com.namoadigital.prj001.dao
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteException
+import com.namoa_digital.namoa_library.util.HMAux
+import com.namoadigital.prj001.database.CursorToHMAuxMapper
+import com.namoadigital.prj001.database.Mapper
+import com.namoadigital.prj001.model.DaoObjReturn
+import com.namoadigital.prj001.model.MdTag
+import com.namoadigital.prj001.util.Constant
+import com.namoadigital.prj001.util.ToolBox_Con
+import com.namoadigital.prj001.util.ToolBox_Inf
+import java.util.*
+
+public class MdTagDao(
+        val context: Context,
+        val mDB_NAME: String,
+        val mDB_VERSION: Int
+) : BaseDao(
+        context, mDB_NAME, mDB_VERSION, Constant.DB_MODE_MULTI
+), DaoWithReturn<MdTag> {
+
+    companion object{
+        const val TABLE = "md_tag"
+        const val CUSTOMER_CODE = "customer_code"
+        const val TAG_CODE = "tag_code"
+        const val TAG_ID = "tag_id"
+        const val TAG_DESC = "tag_desc"
+    }
+
+    private val toMdTagMapper: Mapper<Cursor, MdTag>
+    private val toContentValuesMapper: Mapper<MdTag, ContentValues>
+
+    init {
+
+        toMdTagMapper = CursorToMdTagMapper()
+        toContentValuesMapper = MdTagToContentValuesMapper()
+
+    }
+
+    override fun addUpdate(mdTag: MdTag?): DaoObjReturn {
+        var daoObjReturn = DaoObjReturn()
+        var addUpdateRet: Long = 0
+        var curAction = DaoObjReturn.INSERT_OR_UPDATE
+        //
+        openDB()
+
+        try {
+            daoObjReturn.table = TABLE
+            curAction = DaoObjReturn.UPDATE
+            //Where para update
+            val sbWhere: StringBuilder = getWherePkClause(mdTag)
+            //Tenta update e armazena retorno
+            addUpdateRet = db.update(TABLE, toContentValuesMapper.map(mdTag), sbWhere.toString(), null).toLong()
+            //Se nenhuma linha afetada, tenta insert
+            if (addUpdateRet == 0L) {
+                curAction = DaoObjReturn.INSERT
+                db.insertOrThrow(TABLE, null, toContentValuesMapper.map(mdTag))
+            }
+        } catch (e: SQLiteException) {
+            //Chama metodo que baseado na exception gera obj de retorno setado como erro
+            //e contendo msg de erro tratada.
+            daoObjReturn = ToolBox_Con.getSQLiteErrorCodeDescription(e.message)
+            //Gera arquivo de exception usando dados da exception e do obj de retorno
+            ToolBox_Inf.registerException(
+                    javaClass.name,
+                    Exception(
+                            """
+                ${e.message}
+                ${daoObjReturn.errorMsg}
+                """.trimIndent()
+                    )
+            )
+        } catch (e: Exception) {
+            //Seta obj de retorno com flag de erro e gera arquivo de exception
+            daoObjReturn.setError(true)
+            ToolBox_Inf.registerException(javaClass.name, e)
+        } finally {
+            daoObjReturn.action = curAction
+            daoObjReturn.actionReturn = addUpdateRet
+        }
+        //
+        closeDB()
+        //
+        return daoObjReturn
+    }
+
+    @Throws(java.lang.Exception::class)
+    private fun getWherePkClause(mdTag: MdTag?): StringBuilder {
+        mdTag?.let{
+            return java.lang.StringBuilder()
+                    .append("""
+                        $CUSTOMER_CODE = '${mdTag.customer_code}'  
+                        AND ${TAG_CODE} = '${mdTag.tag_code}'
+                        """.trimIndent()
+                    )
+        }
+        throw Exception("NULL_OBJ_RECEIVED")
+    }
+
+    override fun addUpdate(mdTags: MutableList<MdTag>?, status: Boolean): DaoObjReturn {
+        var daoObjReturn = DaoObjReturn()
+        var addUpdateRet: Long = 0
+        var curAction = DaoObjReturn.INSERT_OR_UPDATE
+        //
+        openDB()
+
+        try {
+            daoObjReturn.table = TABLE
+            //
+            db.beginTransaction()
+
+            if (status) {
+                db.delete(TABLE, null, null)
+            }
+            //
+            mdTags?.forEach{ mdTag ->
+                curAction = DaoObjReturn.UPDATE
+                //Where para update
+                val sbWhere: StringBuilder = getWherePkClause(mdTag)
+                //Tenta update e armazena retorno
+                addUpdateRet = db.update(TABLE, toContentValuesMapper.map(mdTag), sbWhere.toString(), null).toLong()
+                //Se nenhuma linha afetada, tenta insert
+                if (addUpdateRet == 0L) {
+                    curAction = DaoObjReturn.INSERT
+                    db.insertOrThrow(TABLE, null, toContentValuesMapper.map(mdTag))
+                }
+            }
+            //
+            db.setTransactionSuccessful()
+        } catch (e: SQLiteException) {
+            //Chama metodo que baseado na exception gera obj de retorno setado como erro
+            //e contendo msg de erro tratada.
+            daoObjReturn = ToolBox_Con.getSQLiteErrorCodeDescription(e.message)
+            //Gera arquivo de exception usando dados da exception e do obj de retorno
+            ToolBox_Inf.registerException(
+                    javaClass.name,
+                    Exception(
+                            """
+                ${e.message}
+                ${daoObjReturn.errorMsg}
+                """.trimIndent()
+                    )
+            )
+        } catch (e: Exception) {
+            //Seta obj de retorno com flag de erro e gera arquivo de exception
+            daoObjReturn.setError(true)
+            ToolBox_Inf.registerException(javaClass.name, e)
+        } finally {
+            db.endTransaction()
+            daoObjReturn.action = curAction
+            daoObjReturn.actionReturn = addUpdateRet
+        }
+        //
+        closeDB()
+        //
+        return daoObjReturn
+    }
+
+    override fun addUpdate(sQuery: String?) {
+        openDB()
+        try {
+            db.execSQL(sQuery)
+        } catch (e: java.lang.Exception) {
+            ToolBox_Inf.registerException(javaClass.name, e);
+        } finally {
+        }
+        closeDB()
+    }
+
+    override fun remove(sQuery: String?) {
+        openDB()
+        try {
+            db.execSQL(sQuery)
+        } catch (e: java.lang.Exception) {
+            ToolBox_Inf.registerException(javaClass.name, e);
+        } finally {
+        }
+        closeDB()
+    }
+
+    override fun getByString(sQuery: String?): MdTag? {
+        var mdTag: MdTag? = null
+        openDB()
+        try {
+            val cursor = db.rawQuery(sQuery, null)
+            while (cursor.moveToNext()) {
+                mdTag = toMdTagMapper.map(cursor)
+            }
+            //
+            cursor.close()
+        } catch (e: java.lang.Exception) {
+            ToolBox_Inf.registerException(javaClass.name, e)
+        } finally {
+        }
+        closeDB()
+
+        return mdTag
+    }
+
+    override fun getByStringHM(sQuery: String?): HMAux? {
+        var hmAux: HMAux? = null
+        openDB()
+
+        try {
+            val cursor = db.rawQuery(sQuery, null)
+            while (cursor.moveToNext()) {
+                hmAux = CursorToHMAuxMapper.mapN(cursor)
+            }
+            cursor.close()
+        } catch (e: java.lang.Exception) {
+            ToolBox_Inf.registerException(javaClass.name, e)
+        } finally {
+        }
+
+        closeDB()
+
+        return hmAux
+    }
+
+    override fun query(sQuery: String?): MutableList<MdTag> {
+        var mdTags = mutableListOf<MdTag>()
+        openDB()
+        try {
+            val cursor = db.rawQuery(sQuery, null)
+            while (cursor.moveToNext()) {
+                val uAux: MdTag = toMdTagMapper.map(cursor)
+                mdTags.add(uAux)
+            }
+            //
+            cursor.close()
+        } catch (e: java.lang.Exception) {
+            ToolBox_Inf.registerException(javaClass.name, e)
+        } finally {
+        }
+        //
+        closeDB()
+        return mdTags
+    }
+
+    override fun query_HM(sQuery: String?): MutableList<HMAux> {
+        val mdTags: MutableList<HMAux> = ArrayList()
+        openDB()
+        try {
+            val cursor = db.rawQuery(sQuery, null)
+            while (cursor.moveToNext()) {
+                mdTags.add(CursorToHMAuxMapper.mapN(cursor))
+            }
+            cursor.close()
+        } catch (e: java.lang.Exception) {
+            ToolBox_Inf.registerException(javaClass.name, e)
+        } finally {
+        }
+        closeDB()
+        return mdTags
+    }
+
+    class MdTagToContentValuesMapper : Mapper<MdTag, ContentValues> {
+        override fun map(mdTag: MdTag?): ContentValues {
+            val contentValues = ContentValues()
+            mdTag?.let {
+                with(contentValues){
+                    if(mdTag.customer_code > -1){
+                        put(CUSTOMER_CODE, mdTag.customer_code)
+                    }
+                    if(mdTag.tag_code > -1){
+                        put(TAG_CODE, mdTag.tag_code)
+                    }
+                    put(TAG_ID, mdTag.tag_id)
+                    put(TAG_DESC, mdTag.tag_desc)
+                }
+            }
+            return contentValues
+        }
+
+    }
+
+    class CursorToMdTagMapper : Mapper<Cursor, MdTag> {
+        override fun map(cursor: Cursor?): MdTag? {
+            cursor?.let{
+                with(cursor) {
+                    return MdTag(
+                            getInt(getColumnIndex(CUSTOMER_CODE)),
+                            getInt(getColumnIndex(TAG_CODE)),
+                            getString(getColumnIndex(TAG_ID)),
+                            getString(getColumnIndex(TAG_DESC)),
+                    )
+                }
+            }
+            //
+            return null
+        }
+    }
+}
---
+++ app/src/main/java/com/namoadigital/prj001/model/MD_TagJv.java
@@ -0,0 +1,40 @@
+package com.namoadigital.prj001.model;
+
+public class MD_TagJv {
+    private int customer_code;
+    private int tag_code;
+    private int tag_id;
+    private String tag_desc;
+
+    public int getCustomer_code() {
+        return customer_code;
+    }
+
+    public void setCustomer_code(int customer_code) {
+        this.customer_code = customer_code;
+    }
+
+    public int getTag_code() {
+        return tag_code;
+    }
+
+    public void setTag_code(int tag_code) {
+        this.tag_code = tag_code;
+    }
+
+    public int getTag_id() {
+        return tag_id;
+    }
+
+    public void setTag_id(int tag_id) {
+        this.tag_id = tag_id;
+    }
+
+    public String getTag_desc() {
+        return tag_desc;
+    }
+
+    public void setTag_desc(String tag_desc) {
+        this.tag_desc = tag_desc;
+    }
+}
