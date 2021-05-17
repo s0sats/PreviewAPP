8fee03b6025ebf6c6c1a65442b7f81f15e0f0e84
LBarrionuevo <l.barrionuevo@namoadigital.com>
2021-05-17 14:04:15 -0300

    I_Frg_Ticket_Search.java     - INterface do fragmento. @#$

---
+++ app/src/main/java/com/namoadigital/prj001/view/frag/frg_ticket_search/I_Frg_Ticket_Search.java
@@ -0,0 +1,7 @@
+package com.namoadigital.prj001.view.frag.frg_ticket_search;
+
+import com.namoa_digital.namoa_library.util.HMAux;
+
+public interface I_Frg_Ticket_Search {
+    void onSearchClick(String btn_Action, HMAux optionsInfo);
+}
