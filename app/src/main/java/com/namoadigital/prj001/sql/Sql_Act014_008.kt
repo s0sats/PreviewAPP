1d287024a5575a2c05853a9b831f136651ece59f
Daniel <d.luche@namoadigital.com>
2021-06-08 15:56:44 -0300

    Sql_Act014_008.kt  - Modificao query, para não contabilizar form finalizados de ticket e agendamento, não contabilizar ticket com prefixo <= 0

--- app/src/main/java/com/namoadigital/prj001/sql/Sql_Act014_008.kt
+++ app/src/main/java/com/namoadigital/prj001/sql/Sql_Act014_008.kt
@@ -3,6 +3,7 @@ package com.namoadigital.prj001.sql
 import com.namoa_digital.namoa_library.util.HMAux
 import com.namoadigital.prj001.dao.GE_Custom_Form_ApDao
 import com.namoadigital.prj001.dao.GE_Custom_Form_LocalDao
+import com.namoadigital.prj001.dao.MD_Schedule_ExecDao
 import com.namoadigital.prj001.dao.TK_TicketDao
 import com.namoadigital.prj001.database.Specification
 import com.namoadigital.prj001.ui.act014.Act014_Main
@@ -27,35 +28,58 @@ class Sql_Act014_008(
      ifnull(sum(${SENT_QTY}), 0) $SENT_QTY,
      '${label_translation[Act014_Main.LABEL_TRANS_MY_ACTION]}' $TYPE
     FROM (
-         SELECT
-             count(1) ${SENT_QTY}    
-         FROM  ${GE_Custom_Form_ApDao.TABLE} a
-         WHERE a.${GE_Custom_Form_ApDao.CUSTOMER_CODE} = '$customerCode' 
-           AND a.${GE_Custom_Form_ApDao.AP_STATUS}    in ('${Constant.SYS_STATUS_DONE}','${Constant.SYS_STATUS_CANCELLED}')
-               
-         union all    
-         SELECT
-             count(1) ${SENT_QTY}
-         FROM  ${GE_Custom_Form_LocalDao.TABLE} l
-          WHERE l.${GE_Custom_Form_LocalDao.CUSTOMER_CODE} = '$customerCode' 
-            AND l.${GE_Custom_Form_LocalDao.CUSTOM_FORM_STATUS} in (
-                                                '${Constant.SYS_STATUS_DONE}',
-                                                '${Constant.SYS_STATUS_NOT_EXECUTED}',
-                                                '${Constant.SYS_STATUS_CANCELLED}',
-                                                '${Constant.SYS_STATUS_IGNORED}'
-                                                )
-         union all
-                
-            SELECT
-                 count(1) ${SENT_QTY} 
-              FROM  ${TK_TicketDao.TABLE} s
-             WHERE s.customer_code = '$customerCode'
-               and s.ticket_status in('${ConstantBaseApp.SYS_STATUS_DONE}'
-            ,'${ConstantBaseApp.SYS_STATUS_NOT_EXECUTED}'
-            ,'${ConstantBaseApp.SYS_STATUS_CANCELLED}'
-            ,'${ConstantBaseApp.SYS_STATUS_IGNORED}'
-            ,'${ConstantBaseApp.SYS_STATUS_REJECTED}') 
-    )
+             SELECT
+                 count(1) ${SENT_QTY}    
+             FROM  ${GE_Custom_Form_ApDao.TABLE} a
+             WHERE a.${GE_Custom_Form_ApDao.CUSTOMER_CODE} = '$customerCode' 
+               AND a.${GE_Custom_Form_ApDao.AP_STATUS}    in ('${Constant.SYS_STATUS_DONE}','${Constant.SYS_STATUS_CANCELLED}')
+                   
+             UNION ALL    
+             
+             SELECT
+                 count(1) ${SENT_QTY}
+             FROM  ${GE_Custom_Form_LocalDao.TABLE} l
+             WHERE l.${GE_Custom_Form_LocalDao.CUSTOMER_CODE} = '$customerCode' 
+                AND l.${GE_Custom_Form_LocalDao.CUSTOM_FORM_STATUS} in (
+                                                    '${Constant.SYS_STATUS_DONE}',
+                                                    '${Constant.SYS_STATUS_NOT_EXECUTED}',
+                                                    '${Constant.SYS_STATUS_CANCELLED}',
+                                                    '${Constant.SYS_STATUS_IGNORED}'
+                                                    )
+                AND l.${GE_Custom_Form_LocalDao.SCHEDULE_PREFIX} is null
+                AND l.${GE_Custom_Form_LocalDao.SCHEDULE_CODE} is null				
+                AND l.${GE_Custom_Form_LocalDao.SCHEDULE_EXEC} is null				
+                AND l.${GE_Custom_Form_LocalDao.TICKET_PREFIX} is null				
+                AND l.${GE_Custom_Form_LocalDao.TICKET_CODE} is null			
+                AND l.${GE_Custom_Form_LocalDao.TICKET_SEQ} is null				
+                AND l.${GE_Custom_Form_LocalDao.TICKET_SEQ_TMP} is null		
+                AND l.${GE_Custom_Form_LocalDao.STEP_CODE} is null
+                    
+             UNION ALL                
+                SELECT
+                     count(1) ${SENT_QTY} 
+                  FROM  ${TK_TicketDao.TABLE} s
+                 WHERE s.customer_code = '$customerCode'
+                   and s.ticket_prefix > 0
+                   and s.ticket_status in('${ConstantBaseApp.SYS_STATUS_DONE}'
+                                            ,'${ConstantBaseApp.SYS_STATUS_NOT_EXECUTED}'
+                                            ,'${ConstantBaseApp.SYS_STATUS_CANCELLED}'
+                                            ,'${ConstantBaseApp.SYS_STATUS_IGNORED}'
+                                            ,'${ConstantBaseApp.SYS_STATUS_REJECTED}') 
+             
+             UNION ALL   
+                SELECT
+                   count(1) ${SENT_QTY} 
+                FROM
+                   ${MD_Schedule_ExecDao.TABLE} s
+                WHERE 
+                    s.${MD_Schedule_ExecDao.STATUS} in( '${ConstantBaseApp.SYS_STATUS_DONE}',
+                                                        '${ConstantBaseApp.SYS_STATUS_NOT_EXECUTED}',
+                                                        '${ConstantBaseApp.SYS_STATUS_CANCELLED}',
+                                                        '${ConstantBaseApp.SYS_STATUS_ERROR}',          
+                                                        '${ConstantBaseApp.SYS_STATUS_IGNORED}'
+                                                        )                              
+        )
 ;""")
                 .toString()
         return result
