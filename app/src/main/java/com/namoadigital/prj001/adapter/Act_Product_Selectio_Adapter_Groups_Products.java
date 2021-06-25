3602379bbc3571dc55a5a19f3621308b6780d8ef
Daniel <d.luche@namoadigital.com>
2021-06-25 14:25:40 -0300

    Act007_Adapter_Groups_Products e Act_Product_Selectio_Adapter_Groups_Products  - Comentado a view de code e a utilização da informação de codigo do adapter  - Comentado o lbl de tradução do ID e removido espaço na inicialização da var idText  - No xml das classes, ocultado visibilidade do tv_code e modificado o gravity da view de id para ficar alinhada a esquerda @#$

--- app/src/main/java/com/namoadigital/prj001/adapter/Act_Product_Selectio_Adapter_Groups_Products.java
+++ app/src/main/java/com/namoadigital/prj001/adapter/Act_Product_Selectio_Adapter_Groups_Products.java
@@ -66,8 +66,8 @@ public class Act_Product_Selectio_Adapter_Groups_Products extends BaseAdapter {
         ImageView iv_001 = (ImageView)
                 convertView.findViewById(R.id.act_product_selection_content_cell_iv_001);
 
-        TextView tv_code = (TextView)
-                convertView.findViewById(R.id.act_product_selection_content_cell_tv_code);
+//        TextView tv_code = (TextView)
+//                convertView.findViewById(R.id.act_product_selection_content_cell_tv_code);
 
         TextView tv_id = (TextView)
                 convertView.findViewById(R.id.act_product_selection_content_cell_tv_id);
@@ -77,13 +77,13 @@ public class Act_Product_Selectio_Adapter_Groups_Products extends BaseAdapter {
 
         ll_fundo.setBackground(null);
 
-        String codeText = hmAux_Trans.get("lbl_code") + " " ;
-        String idText =  hmAux_Trans.get("lbl_id") + " " ;
+        //String codeText = hmAux_Trans.get("lbl_code") + "" ;
+        String idText =  /*hmAux_Trans.get("lbl_id") + */"" ;
         String descText =  /*hmAux_Trans.get("lbl_desc") + " "*/ "" ;
 
         ColorStateList filterColor =  context.getResources().getColorStateList(R.color.namoa_color_light_blue7);
         //
-        tv_code.setTextColor(filterColor);
+        //tv_code.setTextColor(filterColor);
         tv_id.setTextColor(filterColor);
         tv_desc.setTextColor(filterColor);
         //
@@ -94,11 +94,11 @@ public class Act_Product_Selectio_Adapter_Groups_Products extends BaseAdapter {
             iv_001.setImageResource(R.drawable.ic_pasta);
             iv_001.setColorFilter(context.getResources().getColor(R.color.namoa_color_light_blue7));
             //
-            codeText += item.get("code");
+            //codeText += item.get("code");
             idText += item.get("id");
             descText += item.get("desc");
             //
-            tv_code.setText(codeText);
+            //tv_code.setText(codeText);
             tv_id.setText(idText);
             tv_desc.setText(descText);
 
@@ -107,11 +107,11 @@ public class Act_Product_Selectio_Adapter_Groups_Products extends BaseAdapter {
             iv_001.setImageResource(R.drawable.ic_produto);
             iv_001.setColorFilter(context.getResources().getColor(R.color.namoa_color_light_green5));
             //
-            codeText += item.get("code");
+            //codeText += item.get("code");
             idText += item.get("id");
             descText += item.get("desc");
             //
-            tv_code.setText(codeText);
+            //tv_code.setText(codeText);
             tv_id.setText(idText);
             tv_desc.setText(descText);
 
