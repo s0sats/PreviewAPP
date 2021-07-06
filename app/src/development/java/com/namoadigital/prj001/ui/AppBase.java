a47f6d22cb243f290c1d16c0016374bbba02f079
Daniel <d.luche@namoadigital.com>
2021-07-06 15:09:46 -0300

    AppBase  - Modificado versão do banco para 62 @#$ build.gradle  - Modificado versão para publicação

--- app/src/development/java/com/namoadigital/prj001/ui/AppBase.java
+++ app/src/development/java/com/namoadigital/prj001/ui/AppBase.java
@@ -131,7 +131,7 @@ public class AppBase extends Application {
         DB_FULL_BASE = DB_PATH + "/" + DB_NAME_BASE;
 
         DB_NAME_CUSTOM = "cc.db3";
-        DB_VERSION_CUSTOM = 61;
+        DB_VERSION_CUSTOM = 62;
         DB_FULL_CUSTOM = DB_PATH + "/" + DB_NAME_CUSTOM;
 
         //DB_NAME_CHAT = "namoa_chat.db3";
