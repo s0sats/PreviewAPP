8d6573f35a95aded18aa612d95b619ecad9f4382
LBarrionuevo <l.barrionuevo@namoadigital.com>
2021-11-30 09:07:42 -0300

    Act042SOExpressAdapter.kt     - Novo adapter de SO Express. @#$ MyActionsAdapter.kt ImageVIew.kt     - Desacoplamento de função de extensão. @#$ SO_Pack_Express_Local.java     - Metodo com campos filtrados. @#$ SO_Pack_Express_Local_Sql_009.java     - Query que lista OS expressa no historico. @#$ Act042_Main.java Act042_Main_Presenter_Impl.java Act042MainViewModel.kt     - Alteração para MVVM na Act042. @#$ act042_main_content.xml act042_so_express_cell.xml     - Troca de componente de lista para recyclerview. @#$

---
+++ app/src/main/java/com/namoadigital/prj001/adapter/Act042SOExpressAdapter.kt
@@ -0,0 +1,120 @@
+package com.namoadigital.prj001.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.namoa_digital.namoa_library.util.ConstantBase.SYS_STATUS_SENT
+import com.namoa_digital.namoa_library.util.ToolBox
+import com.namoadigital.prj001.R
+import com.namoadigital.prj001.databinding.Act042SoExpressCellBinding
+import com.namoadigital.prj001.extensions.applyVisibilityIfSourceExists
+import com.namoadigital.prj001.model.SO_Pack_Express_Local
+import com.namoadigital.prj001.util.ToolBox_Inf
+
+class Act042SOExpressAdapter (
+    val soExpressList : ArrayList<SO_Pack_Express_Local>
+) : RecyclerView.Adapter<Act042SOExpressAdapter.MySOExpressVh>(), Filterable{
+    //
+    val soExpressListFiltered: MutableList<SO_Pack_Express_Local> = mutableListOf()
+    //
+    var mFilter = Act042SOExpressFilter()
+    //
+    init {
+        soExpressListFiltered.addAll(soExpressList)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MySOExpressVh {
+        return MySOExpressVh(
+            Act042SoExpressCellBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                ), parent, false
+            )
+        )
+    }
+    //
+    override fun onBindViewHolder(holder: MySOExpressVh, position: Int) {
+        val soPackExpressLocal = soExpressListFiltered[position]
+        holder.onBind(soPackExpressLocal)
+    }
+    //
+    override fun getItemCount() = soExpressListFiltered.size
+    //
+    inner class MySOExpressVh(val binding: Act042SoExpressCellBinding)  : RecyclerView.ViewHolder(binding.root){
+        fun onBind(soPackExpressLocal: SO_Pack_Express_Local){
+            binding.apply {
+                setViewContentAndVisibility(soExpressPackageDesc ,soPackExpressLocal.so_desc)
+                setViewContentAndVisibility(soExpressProductDesc ,soPackExpressLocal.product_desc)
+                setViewContentAndVisibility(soExpressSerialId ,soPackExpressLocal.serial_id)
+                setViewContentAndVisibility(soExpressBillingAddInf1 ,soPackExpressLocal.billing_add_inf1_value)
+                setViewContentAndVisibility(soExpressBillingAddInf2 ,soPackExpressLocal.billing_add_inf2_value)
+                setViewContentAndVisibility(soExpressBillingAddInf3 ,soPackExpressLocal.billing_add_inf3_value)
+                setViewContentAndVisibility(soExpressSiteDesc ,soPackExpressLocal.exec_site_desc)
+                setViewContentAndVisibility(soExpressOperationDesc ,soPackExpressLocal.operation_desc)
+                setViewContentAndVisibility(soExpressErrorMsg ,soPackExpressLocal.ret_msg)
+                setViewContentAndVisibility(soExpressEndDatetime ,ToolBox_Inf.millisecondsToString(
+                    ToolBox_Inf.dateToMilliseconds(soPackExpressLocal.log_date),
+                    ToolBox_Inf.nlsDateFormat(root.context) + " HH:mm"
+                ))
+                if(soPackExpressLocal.status.equals(SYS_STATUS_SENT)) {
+                    soExpressIvStatus.applyVisibilityIfSourceExists(R.drawable.ic_baseline_cloud_done_24_blue)
+                }else{
+                    soExpressIvStatus.applyVisibilityIfSourceExists(R.drawable.ic_cloud_upload_24_red)
+                }
+            }
+        }
+    }
+    //
+
+    fun setViewContentAndVisibility(textView: TextView, content: String?){
+        if(content.isNullOrEmpty()){
+            textView.visibility = View.GONE
+        }else{
+            textView.visibility = View.VISIBLE
+            textView.text = content
+        }
+    }
+
+    override fun getFilter(): Filter {
+        return mFilter
+    }
+    //
+    inner class Act042SOExpressFilter() : Filter(){
+
+        override fun performFiltering(constraint: CharSequence?): FilterResults {
+            var temp = mutableListOf<SO_Pack_Express_Local>()
+            var charFilter = ToolBox.AccentMapper(constraint.toString().toLowerCase())
+            if(charFilter.isNullOrEmpty()){
+                temp = soExpressList as MutableList<SO_Pack_Express_Local>
+            }else{
+                temp.addAll(
+                    soExpressList.filter {
+                        when(it){
+                            is SO_Pack_Express_Local ->{
+                                val allFields = ToolBox.AccentMapper(it.getAllFieldForFilter().toLowerCase())
+                                allFields.contains(charFilter)
+                            }
+                            //se for o botão, sempre exibe
+                            else -> true
+                        }
+                    }
+                )
+            }
+            val filterResults = FilterResults()
+            filterResults.count = temp.size
+            filterResults.values = temp
+            return filterResults
+        }
+
+        override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+            results?.let {
+                soExpressListFiltered.clear()
+                soExpressListFiltered.addAll(results.values as Collection<SO_Pack_Express_Local>)
+                notifyDataSetChanged()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/namoadigital/prj001/ui/act042/Act042MainViewModel.kt
@@ -0,0 +1,38 @@
+package com.namoadigital.prj001.ui.act042
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.namoadigital.prj001.dao.SO_Pack_Express_LocalDao
+import com.namoadigital.prj001.model.SO_Pack_Express_Local
+import com.namoadigital.prj001.sql.SO_Pack_Express_Local_Sql_009
+import com.namoadigital.prj001.util.Constant
+import java.util.*
+
+class Act042MainViewModel(val customerCode: Long, val expressLocalDao: SO_Pack_Express_LocalDao): ViewModel() {
+
+    val so_express_list: MutableLiveData<ArrayList<SO_Pack_Express_Local>> = MutableLiveData<ArrayList<SO_Pack_Express_Local>>().apply {
+        (mutableListOf<SO_Pack_Express_Local>())
+    }
+
+    fun getSoExpressList(){
+        so_express_list.value =
+            expressLocalDao.query(
+                SO_Pack_Express_Local_Sql_009(
+                    customerCode,
+                    Constant.RETURN_SQL_HM_AUX
+                ).toSqlQuery()
+            ) as ArrayList<SO_Pack_Express_Local>?
+    }
+
+}
+
+class Act042MainViewModelFactory(private val customerCode: Long, private val expressLocalDao: SO_Pack_Express_LocalDao) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(Act042MainViewModel::class.java)) {
+            return Act042MainViewModel(customerCode, expressLocalDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
