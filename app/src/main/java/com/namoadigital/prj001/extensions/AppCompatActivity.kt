da19b5d2d21456f92cf5d53287d99ed3f7176479
Daniel Luche <d.luche@namoadigital.com>
2021-12-06 10:36:23 -0300

    AppCompatActivity.Kt  - Add extension fun forceOpenKeyboard que força a abertura do teclado @#$ Act040  - Modificado o clique no clFinalizeBtn, resgatando a string de serial somente uma vez. Modificado o titulo do dialog de confirma para exibir o serial digitado ao inves de um text  - Modificado o clique no clFinalizeBtn, para que no caso de erro de serial invalido, além de setar o foco no mket de serial, abrir o teclado e setar o cursor no final do serial

--- app/src/main/java/com/namoadigital/prj001/extensions/AppCompatActivity.kt
+++ app/src/main/java/com/namoadigital/prj001/extensions/AppCompatActivity.kt
@@ -5,6 +5,9 @@ import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * Fun que esconde o teclado
+ */
 fun AppCompatActivity.hideKeyboard() {
     val view = this.currentFocus
     if (view != null) {
@@ -13,4 +16,15 @@ fun AppCompatActivity.hideKeyboard() {
     }
 
     window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+}
+
+/**
+ * LUCHE - 06/12/2021
+ * Fun que força a abertura do teclado
+ */
+fun AppCompatActivity.forceOpenKeyboard() {
+    (this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).toggleSoftInput(
+        InputMethodManager.SHOW_FORCED,
+        InputMethodManager.HIDE_IMPLICIT_ONLY
+    )
 }
