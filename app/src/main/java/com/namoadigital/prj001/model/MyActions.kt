9238b824b5e06ca59b48d202fece98a27345e056
LBarrionuevo <l.barrionuevo@namoadigital.com>
2022-07-13 14:18:41 -0300

    MyActions.kt     - Adição de comentario interno na busca. @#$

--- app/src/main/java/com/namoadigital/prj001/model/MyActions.kt
+++ app/src/main/java/com/namoadigital/prj001/model/MyActions.kt
@@ -69,6 +69,7 @@ data class MyActions(
                 "$serialId|" +
                 "$originDescriptor|" +
                 "$processDesc|" +
+                "$internalComments|" +
                 "$focusStepDesc|" +
                 "$siteDesc|" +
                 "$zoneDesc|" +
