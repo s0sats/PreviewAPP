dd8688ce8350093f021fff7c3f6f905ec7169e42
choicedev <fabricio_zamora1@outlook.com>
2023-07-17 15:34:20 -0300

    :construction: Adicionando validação de serial na act008 e removendo replace que tinha na act006

--- app/src/main/java/com/namoadigital/prj001/extensions/StringHelper.kt
+++ app/src/main/java/com/namoadigital/prj001/extensions/StringHelper.kt
@@ -1,13 +1,17 @@
 package com.namoadigital.prj001.extensions
 
 import java.text.Normalizer
+import java.util.regex.Pattern
 
-fun String?.formatForDisplay() = if(this.isNullOrBlank()) "" else this
+fun String?.formatForDisplay() = if (this.isNullOrBlank()) "" else this
 
 
-fun String.stripAccents(): String
-{
+fun String.stripAccents(): String {
     var string = Normalizer.normalize(this, Normalizer.Form.NFD)
     string = Regex("\\p{InCombiningDiacriticalMarks}+").replace(string, "")
-    return  string
+    return string
 }
+
+fun checkIfHasCharInvalid(value: String) =
+    Pattern.compile("/^\\s|\\s$|\\s{2}|[\\t\\n\\r]|[^\\s0-9a-zà-ü\\-\\_\\(\\)\\[\\]\\{\\}\\.\\|\\/\\+\\\\]/gmi")
+        .matcher(value).find()
