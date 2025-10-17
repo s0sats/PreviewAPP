a5b444f95475d1c1d0c9961a5e7a49ff97973ef2
Fabricio Zamora <108739083+fabricio-namoa@users.noreply.github.com>
2025-10-17 16:18:41 -0300

    Atualiza a versão do aplicativo (#98)
    - **`app/build.gradle.kts`:**
        - O `versionCode` foi incrementado de `452` para `454`.
        - O `versionName` agora é `6.16.7`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.namoadigital.prj001"
         minSdk = 21
         targetSdk = 35
-        versionCode = 452
+        versionCode = 454
         versionName = "6.16.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
